9e1415b745058cb07c965e94aa688bf96aa1a56a
Robert Strandh <robert.strandh@gmail.com>
2023-03-04 09:55:35 +0100

    Add definition of MACROLET-AST.

--- Code/flet-ast.lisp
+++ Code/flet-ast.lisp
@@ -3,14 +3,17 @@
 (defclass lexical-function-ast (name-ast-mixin function-ast)
   ())
 
-(defclass flet-or-labels-ast
+(defclass flet-or-labels-or-macrolet-ast
     (declaration-asts-mixin form-asts-mixin ast)
   ((%lexical-function-asts
       :initarg :lexical-function-asts
       :reader lexical-function-asts)))
 
-(defclass flet-ast (flet-or-labels-ast)
+(defclass flet-ast (flet-or-labels-or-macrolet-ast)
   ())
 
-(defclass labels-ast (flet-or-labels-ast)
+(defclass labels-ast (flet-or-labels-or-macrolet-ast)
+  ())
+
+(defclass macrolet-ast (flet-or-labels-or-macrolet-ast)
   ())
