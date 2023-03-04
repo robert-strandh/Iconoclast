e98e93ee6b315414f1eef1bf3ff44f5e2e2a3b3f
Robert Strandh <robert.strandh@gmail.com>
2023-03-04 08:28:09 +0100

    Use mixin classes as superclasses of CATCH-AST and THROW-AST.

--- Code/catch-ast.lisp
+++ Code/catch-ast.lisp
@@ -1,21 +1,11 @@
 (cl:in-package #:iconoclast)
 
-(defgeneric tag-ast (ast))
-
-(defclass catch-ast (ast)
-  ((%tag-ast
-      :initarg :tag-ast
-      :reader tag-ast)
-   (%form-asts
-      :initarg :form-asts
-      :reader form-asts)))
+(defclass catch-ast (tag-ast-mixin form-asts-mixin ast)
+  ())
 
 (defgeneric result-form-ast (ast))
 
-(defclass throw-ast (ast)
-  ((%tag-ast
-      :initarg :tag-ast
-      :reader tag-ast)
-   (%result-form-ast
+(defclass throw-ast (tag-ast-mixin ast)
+  ((%result-form-ast
       :initarg :result-form-ast
       :reader result-form-ast)))
