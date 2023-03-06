94e28ba675982d0502dd75a8f060b24b9d04930c
Robert Strandh <robert.strandh@gmail.com>
2023-03-06 07:02:20 +0100

    Export names of FORM-ASTS and VARIABLE-BINDING-ASTS.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -9,11 +9,13 @@
      #:variable-definition-ast
      #:variable-reference-ast
      #:init-form-ast
+     #:form-asts
      #:supplied-p-parameter-ast
      #:optional-parameter-ast
      #:keyword-name-ast
      #:key-parameter-ast
      #:variable-binding-ast
+     #:variable-binding-asts
      #:let-ast
      #:let*-ast
      #:name-ast
