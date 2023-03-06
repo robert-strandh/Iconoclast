0ec65f0dc1c6b1e21dd3cd985bccd88f0499a55f
Robert Strandh <robert.strandh@gmail.com>
2023-03-06 15:57:15 +0100

    Define macro for definining methods on MAKE-NODE.

--- Code/Builder/builder.lisp
+++ Code/Builder/builder.lisp
@@ -2,3 +2,11 @@
 
 (defclass builder ()
   ())
+
+(defmacro define-make-node-method (kind class-name)
+  `(defmethod abp:make-node
+       ((builder builder)
+        (kind (eql ,kind))
+        &key source)
+      (make-instance ',class-name
+        :origin source)))
