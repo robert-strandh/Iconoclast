36897cbe880a1d0298ebaaf1ff593f847659daff
Robert Strandh <robert.strandh@gmail.com>
2023-03-06 07:02:50 +0100

    Provide more methods on MAKE-NODE and RELATE.

--- Code/Builder/builder.lisp
+++ Code/Builder/builder.lisp
@@ -18,6 +18,13 @@
   (make-instance 'ico:variable-binding-ast
     :origin source))
 
+(defmethod abp:make-node
+    ((builder builder)
+     (kind (eql :let))
+     &key source)
+  (make-instance 'ico:let-ast
+    :origin source))
+
 (defmethod abp:relate
     ((builder builder)
      (relation (eql :name))
@@ -37,3 +44,27 @@
   ;; and should be parsed.
   (reinitialize-instance left
     :form-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :binding))
+     (left ico:let-ast)
+     (right ico:variable-binding-ast)
+     &key)
+  (reinitialize-instance left
+    :variable-binding-asts
+      (append (ico:variable-binding-asts left)
+              (list right))))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :form))
+     (left ico:let-ast)
+     (right t)
+     &key)
+  ;; This is wrong since right is a form
+  ;; and should be parsed.
+  (reinitialize-instance left
+    :form-asts
+      (append (ico:form-asts left)
+              (list right))))
