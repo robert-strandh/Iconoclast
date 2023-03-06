3ea844a32ef73d8c4d963682d09f27c597d91b98
Robert Strandh <robert.strandh@gmail.com>
2023-03-06 10:56:41 +0100

    Change slot name to reflect that it contains an AST.

--- Code/multiple-value-call-ast.lisp
+++ Code/multiple-value-call-ast.lisp
@@ -1,8 +1,8 @@
 (cl:in-package #:iconoclast)
 
-(defgeneric function-call-form (ast))
+(defgeneric function-call-form-ast (ast))
 
 (defclass multiple-value-call-ast (form-asts-mixin ast)
-  ((%function-call-form
-      :initarg :function-call-form
-      :reader function-call-form)))
+  ((%function-call-form-ast
+      :initarg :function-call-form-ast
+      :reader function-call-form-ast)))
