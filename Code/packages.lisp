2f1ff9d6731d31cfb3b11167c610e791dd3b3a4a
Robert Strandh <robert.strandh@gmail.com>
2023-03-06 10:55:48 +0100

    Export more classes and slot accessors.

--- Code/packages.lisp
+++ Code/packages.lisp
@@ -8,20 +8,73 @@
      #:variable-ast
      #:variable-definition-ast
      #:variable-reference-ast
+     #:referencing-variable-asts
+     #:defining-variable-ast
+     #:declaration-specifier-ast
+     #:declration-specifier-with-variables-ast
+     #:dynamic-extent-ast
+     #:type-or-ftype-ast
+     #:type-ast
+     #:ftype-ast
+     #:ignore-or-ignorable-ast
+     #:ignore-ast
+     #:ignorable-ast
+     #:inline-or-notinline-ast
+     #:inline-ast
+     #:notinline-ast
+     #:optimize-ast
+     #:declaration-specifier-asts
+     #:declare-ast
      #:init-form-ast
      #:form-asts
      #:supplied-p-parameter-ast
      #:optional-parameter-ast
      #:keyword-name-ast
      #:key-parameter-ast
+     #:lambda-list
      #:variable-binding-ast
      #:variable-binding-asts
      #:let-ast
      #:let*-ast
      #:name-ast
-     #:lambda-list
      #:declare-asts
      #:documentation-ast
      #:body-asts
-     #:declaration-specifiers
-     #:declare-ast))
+     #:block-ast
+     #:return-from-ast
+     #:catch-ast
+     #:result-form-ast
+     #:throw-ast
+     #:situation-asts
+     #:eval-when-ast
+     #:lexical-function-ast
+     #:flet-or-labels-ast
+     #:flet-ast
+     #:labels-ast
+     #:function-ast
+     #:tagbody-ast
+     #:contents-asts
+     #:tagbody-tag-ast
+     #:go-ast
+     #:test-ast
+     #:then-ast
+     #:else-ast
+     #:if-ast
+     #:read-only-p-ast
+     #:load-time-value-ast
+     #:locally-ast
+     #:function-call-form-ast
+     #:multiple-value-call-ast
+     #:first-form-ast
+     #:multiple-value-prog1-ast
+     #:progn-ast
+     #:symbols-ast
+     #:values-ast
+     #:progv-ast
+     #:object-ast
+     #:quote-ast
+     #:setq-ast
+     #:value-type-ast
+     #:the-ast
+     #:protected-form-ast
+     #:unwind-protect-ast))
