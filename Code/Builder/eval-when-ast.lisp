80ebd9f9b716f8f512649e079025edc813f12370
Robert Strandh <robert.strandh@gmail.com>
2023-03-07 19:10:37 +0100

    Remove redundant method now replaced by method on FORM-ASTS-MIXIN.

--- Code/Builder/eval-when-ast.lisp
+++ Code/Builder/eval-when-ast.lisp
@@ -10,12 +10,3 @@
      &key)
   (reinitialize-instance left
     :situation-asts (append (ico:situation-asts left) (list right))))
-
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :form))
-     (left ico:eval-when-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left
-    :form-asts (append (ico:form-asts left) (list right))))
