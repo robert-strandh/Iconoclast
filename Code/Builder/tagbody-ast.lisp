7d53e8801edd82b9acd6f5dd662b6f74b98edbe7
Robert Strandh <robert.strandh@gmail.com>
2023-03-07 14:58:40 +0100

    Add method for TAGBODY-SEGMENT-AST.

--- Code/Builder/tagbody-ast.lisp
+++ Code/Builder/tagbody-ast.lisp
@@ -2,6 +2,8 @@
 
 (define-make-node-method :tagbody ico:tagbody-ast)
 
+(define-make-node-method :tagbody-segment ico:tagbody-segment-ast)
+
 (defmethod abp:relate
     ((builder builder)
      (relation (eql :tag))
@@ -10,3 +12,21 @@
      &key)
   (reinitialize-instance left
     :contents-asts (append (ico:contents-asts left) (list right))))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :label))
+     (left ico:tagbody-segment-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left
+    :tag-asts (append (ico:tag-asts left) (list right))))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :statement))
+     (left ico:tagbody-segment-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left
+    :form-asts (append (ico:form-asts left) (list right))))
