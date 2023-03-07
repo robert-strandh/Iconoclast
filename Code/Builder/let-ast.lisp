b6ab7152f3867e66ce8a6db2d10885432fb5aad2
Robert Strandh <robert.strandh@gmail.com>
2023-03-07 19:08:49 +0100

    Remove redundant method now replaced by method on FORM-ASTS-MIXIN.

--- Code/Builder/let-ast.lisp
+++ Code/Builder/let-ast.lisp
@@ -34,14 +34,3 @@
     :variable-binding-asts
       (append (ico:variable-binding-asts left)
               (list right))))
-
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :form))
-     (left ico:let-or-let*-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left
-    :form-asts
-      (append (ico:form-asts left)
-              (list right))))
