3475857744ed3c26cb2c52ca7680f6d9fbc1d28b
Robert Strandh <robert.strandh@gmail.com>
2023-03-07 08:27:08 +0100

    Define methods on RELATE for THROW-AST.

--- Code/Builder/catch-ast.lisp
+++ Code/Builder/catch-ast.lisp
@@ -20,3 +20,23 @@
   (reinitialize-instance left
     :form-asts
       (append (ico:form-asts left) (list right))))
+
+(define-make-node-method :throw ico:throw-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :tag-form))
+     (left ico:throw-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left
+    :tag-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :result-form))
+     (left ico:throw-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left
+    :result-form-ast right))
