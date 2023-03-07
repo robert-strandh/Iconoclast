c85c93db293adace8f424f3c5455eaad1d0f0cc6
Robert Strandh <robert.strandh@gmail.com>
2023-03-07 14:57:50 +0100

    Add definition of class TAGBODY-SEGMENT-AST and its slot accessor.

--- Code/tagbody-ast.lisp
+++ Code/tagbody-ast.lisp
@@ -6,6 +6,15 @@
       :initarg :contents-asts
       :reader contents-asts)))
 
+(defgeneric tag-asts (ast))
+
+(defclass tagbody-segment-ast (form-asts-mixin ast)
+  ((%tag-asts
+      :initform '()
+      :initarg :tag-asts
+      :reader tag-asts)))
+
+
 (defclass tagbody-tag-ast (tag-ast-mixin ast)
   ((%tagbody-ast
       :initarg :tagbody-ast
