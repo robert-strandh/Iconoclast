155e08780c922d3677a705fbc549f883831c4983
Robert Strandh <robert.strandh@gmail.com>
2023-03-09 11:13:13 +0100

    DEfine methods for ATOMIC-TYPE-SPECIFIER-AST..

--- Code/Builder/type-specifier-asts.lisp
+++ Code/Builder/type-specifier-asts.lisp
@@ -1,3 +1,14 @@
 (cl:in-package #:iconoclast-builder)
 
 (define-make-node-method :type-name ico:type-name-ast)
+
+(define-make-node-method :atomic-type-specifier ico:atomic-type-specifier-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :name))
+     (left ico:atomic-type-specifier-ast)
+     (right ico:type-name-ast)
+     &key)
+  (reinitialize-instance left
+    :name-ast right))
