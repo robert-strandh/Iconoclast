a6009a3065e421e3a4a1ed402423ae322389b473
Robert Strandh <robert.strandh@gmail.com>
2023-03-11 16:43:53 +0100

    New component containing definitions of generic functions.

---
+++ Code/Print-tree/generic-functions.lisp
@@ -0,0 +1,11 @@
+(cl:in-package #:iconoclast-print-tree)
+
+(defgeneric print-node (stream depth node))
+
+(defmethod print-node (stream depth (node ico:ast))
+  (format stream "~a" (class-name node)))
+
+(defgeneric print-details (stream depth node)
+      (:method-combination progn :most-specific-last))
+
+(defgeneric children (node))
