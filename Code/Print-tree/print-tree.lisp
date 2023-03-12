c1d167c01125318ff4c5a3e2da0393896fd01529
Robert Strandh <robert.strandh@gmail.com>
2023-03-12 12:01:16 +0100

    Change the contract of CHILDREN to return a pair.

--- Code/Print-tree/print-tree.lisp
+++ Code/Print-tree/print-tree.lisp
@@ -1,9 +1,17 @@
 (cl:in-package #:iconoclast-print-tree)
 
+(defun print-pair (stream depth pair)
+  (unless (equal (car pair) "")
+    (format stream "~a: " (car pair)))
+  (print-node stream depth (cdr pair)))
+
+(defun children-of-pair (pair)
+  (children (cdr pair)))
+
 (defun print-tree (ast)
   (fresh-line)
   (terpri)
   (utilities.print-tree:print-tree
-   *standard-output* ast
+   *standard-output* (cons "" ast)
    (utilities.print-tree:make-node-printer
-    #'print-node nil #'children)))
+    #'print-pair nil #'children-of-pair)))
