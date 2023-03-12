b92ff0fa21e563be81ed4c2d46c7f0cd2c7a4351
Robert Strandh <robert.strandh@gmail.com>
2023-03-12 10:02:16 +0100

    Provide method on MAKE-NODE to create a BLOCK-NAME-AST.

--- Code/Builder/block-ast.lisp
+++ Code/Builder/block-ast.lisp
@@ -2,6 +2,14 @@
 
 (define-make-node-method :block ico:block-ast)
 
+(defmethod abp:make-node
+    ((builder builder)
+     (kind (eql :block-name))
+     &key source name)
+  (make-instance 'ico:block-name-ast
+    :origin source
+    :name name))
+
 (defmethod abp:relate
     ((builder builder)
      (relation (eql :name))
