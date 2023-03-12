2fbed2f10426c0bf24b1c1937452b85726306cc4
Robert Strandh <robert.strandh@gmail.com>
2023-03-12 10:02:51 +0100

    Bug fix: take the CLASS-OF the AST before calling CLASS-NAME.

--- Code/Print-tree/generic-functions.lisp
+++ Code/Print-tree/generic-functions.lisp
@@ -3,7 +3,7 @@
 (defgeneric print-node (stream depth node))
 
 (defmethod print-node (stream depth (node ico:ast))
-  (format stream "~a" (class-name node)))
+  (format stream "~a" (class-name (class-of node))))
 
 (defgeneric print-details (stream depth node)
       (:method-combination progn :most-specific-last))
