18f11a51a1a768c978678eda594ff2c6e1303907
Robert Strandh <robert.strandh@gmail.com>
2023-03-12 14:12:38 +0100

    New components with methods for declarations.

---
+++ Code/Builder/declare-ast.lisp
@@ -0,0 +1,27 @@
+(cl:in-package #:iconoclast-builder)
+
+(defmethod abp:make-node
+    ((builder builder)
+     (kind (eql :declaration))
+     &key source ((:kind declaration-kind)))
+  (make-instance
+      (case declaration-kind
+        (dynamic-extent 'ico:dynamic-extent-ast)
+        (type 'ico:type-ast)
+        (ftype 'ico:ftype-ast)
+        (ignore 'ico:ignore-ast)
+        (ignorable 'ico:ignorable-ast)
+        (inline 'ico:inline-ast)
+        (notinline 'ico:notinline-ast)
+        (optimize 'ico:optimize-ast)
+        #+(or)(declaration 'ico:declaration-ast))
+    :origin source))
+
+(defmethod abp:relate
+    ((builder builder)
+     (kind (eql :argument))
+     (left ico:inline-or-notinline-ast)
+     (right ico:function-name-ast)
+     &key)
+  (reinitialize-instance left
+    :variable-asts (append (ico:variable-asts left) (list right))))
