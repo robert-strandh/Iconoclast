d0b4eb82c971b176c8f2191ea32b1cc39647029d
Robert Strandh <robert.strandh@gmail.com>
2023-03-12 12:02:30 +0100

    Return a list of the two child pairs in method on CHILDREN.

--- Code/Print-tree/block-ast.lisp
+++ Code/Print-tree/block-ast.lisp
@@ -1,7 +1,8 @@
 (cl:in-package #:iconoclast-print-tree)
 
 (defmethod children ((ast ico:block-ast))
-  (cons (ico:name-ast ast) (ico:form-asts ast)))
+  (list (cons "name-ast" (ico:name-ast ast))
+        (cons "form-asts" (ico:form-asts ast))))
 
 ;;; FIXME: move this one?
 (defmethod children ((ast ico:name-ast))
