fa6c61e241fd575fa92e6c42ecd720b501897c1d
Robert Strandh <robert.strandh@gmail.com>
2023-03-13 11:24:48 +0100

    Define generic function CHILDREN

--- Code/ast.lisp
+++ Code/ast.lisp
@@ -11,3 +11,19 @@
 ;;; The base class for all ASTs.
 (defclass ast ()
   ((%origin :initarg :origin :initform nil :reader origin)))
+
+;;; This generic function returns a list of pairs.  The CAR of each
+;;; pair is a string that names the child in question, and the CDR is
+;;; the child AST.  We use the APPEND method combination so that each
+;;; class supplies a method that returns a list of only the children
+;;; that that class contributes.
+(defgeneric children (ast)
+  (:method-combination append :most-specific-last))
+
+;;; The default method returns the empty list.
+(defmethod children (ast)
+  '())
+
+(defmethod children append ((node cons))
+  (loop for element in node
+        collect (cons "" element)))
