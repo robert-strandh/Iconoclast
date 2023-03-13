d28f9643e618ca349fd5b5fdb4cf3455dbcd46da
Robert Strandh <robert.strandh@gmail.com>
2023-03-13 14:39:48 +0100

    Supply method qualifier PROGN for PRINT-DETAILS.

--- Code/Print-tree/print-details.lisp
+++ Code/Print-tree/print-details.lisp
@@ -4,8 +4,8 @@
     (stream deptn (node iconoclast-builder:unparsed-form-ast))
   (format stream "Form: ~S~@:_" (iconoclast-builder:form node)))
 
-(defmethod print-details (stream depth (node ico:tag-ast))
+(defmethod print-details progn (stream depth (node ico:tag-ast))
   (format stream "Tag: ~a~@:_" (ico:tag node)))
 
-(defmethod print-details (stream depth (node ico:name-ast))
+(defmethod print-details progn (stream depth (node ico:name-ast))
   (format stream "Name: ~a~@:_" (ico:name node)))
