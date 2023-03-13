ea192b57c4826e5630de8a6fa9579d6779327254
Robert Strandh <robert.strandh@gmail.com>
2023-03-13 07:48:43 +0100

    Print the unparsed form as part of details.

--- Code/Print-tree/unparsed.lisp
+++ Code/Print-tree/unparsed.lisp
@@ -2,3 +2,7 @@
 
 (defmethod children ((ast iconoclast-builder:unparsed-form-ast))
   '())
+
+(defmethod print-details progn
+    (stream deptn (node iconoclast-builder:unparsed-form-ast))
+  (format stream "Form: ~S~@:_" (iconoclast-builder:form node)))
