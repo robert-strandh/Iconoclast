774c78317cc2c14001ee39fb8243a7c6e857676c
Robert Strandh <robert.strandh@gmail.com>
2023-03-13 11:34:57 +0100

    Remove method on CHILDREN

--- Code/Print-tree/unparsed.lisp
+++ Code/Print-tree/unparsed.lisp
@@ -1,8 +1,5 @@
 (cl:in-package #:iconoclast-print-tree)
 
-(defmethod children ((ast iconoclast-builder:unparsed-form-ast))
-  '())
-
 (defmethod print-details progn
     (stream deptn (node iconoclast-builder:unparsed-form-ast))
   (format stream "Form: ~S~@:_" (iconoclast-builder:form node)))
