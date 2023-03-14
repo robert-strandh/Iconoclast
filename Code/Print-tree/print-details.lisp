883b856be34b3a5bc1a974416b34aa0210dc485d
Robert Strandh <robert.strandh@gmail.com>
2023-03-14 12:31:11 +0100

    Remove obsolete method on PRINT-DETAILS.

--- Code/Print-tree/print-details.lisp
+++ Code/Print-tree/print-details.lisp
@@ -4,9 +4,6 @@
     (stream deptn (node iconoclast-builder:unparsed-form-ast))
   (format stream "Form: ~S~@:_" (iconoclast-builder:form node)))
 
-(defmethod print-details progn (stream depth (node ico:tag-ast))
-  (format stream "Tag: ~a~@:_" (ico:tag node)))
-
 (defmethod print-details progn (stream depth (node ico:name-mixin))
   (format stream "~a~@:_" (ico:name node)))
 
