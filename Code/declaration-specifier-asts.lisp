982fbee5c465973f6cac90ce7fd162799c63f1ab
Robert Strandh <robert.strandh@gmail.com>
2023-03-14 08:12:59 +0100

    Make declaration specifier ASTs use NAME-ASTS-MIXIN as superclass

--- Code/declaration-specifier-asts.lisp
+++ Code/declaration-specifier-asts.lisp
@@ -3,20 +3,10 @@
 (defclass declaration-specifier-ast (ast)
   ())
 
-(defclass declaration-specifier-with-variables-ast
-    (declaration-specifier-ast)
-  (;; This slot contains a( possibly empty) list of VARIABLE- ASTs.
-   (%variable-asts
-      :initform '()
-      :initarg :variable-asts
-      :reader variable-asts)))
-
-(defclass dynamic-extent-ast
-    (declaration-specifier-with-variables-ast)
+(defclass dynamic-extent-ast (name-asts-mixin declaration-specifier-ast)
   ())
 
-(defclass type-or-ftype-ast
-    (declaration-specifier-with-variables-ast)
+(defclass type-or-ftype-ast (name-asts-mixin declaration-specifier-ast)
   ((%type-specifier-ast
       :initarg :type-specifier-ast
       :reader type-specifier-ast)))
@@ -27,8 +17,7 @@
 (defclass ftype-ast (type-or-ftype-ast)
   ())
 
-(defclass ignore-or-ignorable-ast
-    (declaration-specifier-with-variables-ast)
+(defclass ignore-or-ignorable-ast (name-asts-mixin declaration-specifier-ast)
   ())
 
 (defclass ignore-ast (ignore-or-ignorable-ast)
@@ -37,8 +26,7 @@
 (defclass ignorable-ast (ignore-or-ignorable-ast)
   ())
 
-(defclass inline-or-notinline-ast
-    (declaration-specifier-with-variables-ast)
+(defclass inline-or-notinline-ast (name-asts-mixin declaration-specifier-ast)
   ())
 
 (defclass inline-ast (inline-or-notinline-ast)
