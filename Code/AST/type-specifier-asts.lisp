7978676ebd84f388d4f80bd234f874ac298c1b2a
Robert Strandh <robert.strandh@gmail.com>
2023-03-15 09:04:58 +0100

    Fix incorrect superclass.

--- Code/AST/type-specifier-asts.lisp
+++ Code/AST/type-specifier-asts.lisp
@@ -1,6 +1,6 @@
 (cl:in-package #:iconoclast)
 
-(defclass type-name-ast (name-ast-mixin ast)
+(defclass type-name-ast (name-mixin ast)
   ())
 
 (defclass atomic-type-specifier-ast (name-ast-mixin ast)
