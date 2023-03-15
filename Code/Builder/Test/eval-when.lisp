8b51c35e044a6fb3504611c82b3f887b32e21456
Robert Strandh <robert.strandh@gmail.com>
2023-03-15 17:44:24 +0100

    Add another test for EVAL-WHEN.

--- Code/Builder/Test/eval-when.lisp
+++ Code/Builder/Test/eval-when.lisp
@@ -7,5 +7,14 @@
                      ("form-asts" ((bld:unparsed-form-ast :form 234)))
                      ("situation-asts" nil))))))
 
+(defun test-eval-when-2 ()
+  (let ((result (bld::test '(eval-when (:compile-toplevel) 234))))
+    (assert (equal (convert-ast result)
+                   '(ico:eval-when-ast
+                     ("form-asts" ((bld:unparsed-form-ast :form 234)))
+                     ("situation-asts"
+                      ((ico:situation-ast :name :compile-toplevel))))))))
+
 (defun test-eval-when ()
-  (test-eval-when-1))
+  (test-eval-when-1)
+  (test-eval-when-2))
