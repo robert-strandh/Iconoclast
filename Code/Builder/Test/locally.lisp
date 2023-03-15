caa9973946c416b9e55c1208af32bc24c25fae43
Robert Strandh <robert.strandh@gmail.com>
2023-03-15 08:33:23 +0100

    Add comments and test for an empty list of forms.

--- Code/Builder/Test/locally.lisp
+++ Code/Builder/Test/locally.lisp
@@ -1,10 +1,15 @@
 (cl:in-package #:iconoclast-builder-test)
 
-;;; FIXME: test that the list of FORM-ASTs is empty as well, but
-;;; currently it is not, so wait with that introduction.
+;;; Test that (LOCALLY) generates a LOCALLY-AST with an empty
+;;; list of declarations and an empty list of forms.
 (defun test-locally-1 ()
-  (let ((result (bld::test '(locally ()))))
-    (assert (null (ico:declaration-asts result)))))
+  (let ((result (bld::test '(locally))))
+    (assert (typep result 'ico:locally-ast))
+    (assert (null (ico:declaration-asts result)))
+    (assert (null (ico:form-asts result)))))
 
+;;; None of these tests has any declarations in it.  We test for
+;;; declarations using LOCALLY in a later test when we test the
+;;; parsing of declarations.
 (defun test-locally ()
   (test-locally-1))
