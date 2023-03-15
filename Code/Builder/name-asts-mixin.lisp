4af609bc5f3d7db2f3b12f56e0832411d8c614bc
Robert Strandh <robert.strandh@gmail.com>
2023-03-15 05:57:08 +0100

    Comment out the code for now.

--- Code/Builder/name-asts-mixin.lisp
+++ Code/Builder/name-asts-mixin.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:iconoclast-builder)
 
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :argument))
-     (left ico:name-asts-mixin)
-     (right ico:function-name-ast)
-     &key)
-  (reinitialize-instance left
-    :name-asts (append (ico:name-asts left) (list right))))
+;; (defmethod abp:relate
+;;     ((builder builder)
+;;      (relation (eql :argument))
+;;      (left ico:name-asts-mixin)
+;;      (right ico:function-name-ast)
+;;      &key)
+;;   (reinitialize-instance left
+;;     :name-asts (append (ico:name-asts left) (list right))))
