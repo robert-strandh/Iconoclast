acff09ff9c636038d340419e7469670c083f173e
Robert Strandh <robert.strandh@gmail.com>
2023-03-15 08:40:23 +0100

    Add another test for LOCALLY.

--- Code/Builder/Test/locally.lisp
+++ Code/Builder/Test/locally.lisp
@@ -8,8 +8,19 @@
     (assert (null (ico:declaration-asts result)))
     (assert (null (ico:form-asts result)))))
 
+(defun test-locally-2 ()
+  (let ((result (bld::test '(locally 234))))
+    (assert (typep result 'ico:locally-ast))
+    (assert (null (ico:declaration-asts result)))
+    (let ((form-asts (ico:form-asts result)))
+      (assert (= (length form-asts) 1))
+      (let ((form-ast (first form-asts)))
+        (assert (typep form-ast 'bld:unparsed-form-ast))
+        (assert (eql (bld:form form-ast) 234))))))
+
 ;;; None of these tests has any declarations in it.  We test for
 ;;; declarations using LOCALLY in a later test when we test the
 ;;; parsing of declarations.
 (defun test-locally ()
-  (test-locally-1))
+  (test-locally-1)
+  (test-locally-2))
