ae3256c591d709d372033349147fce5f9da1f380
Robert Strandh <robert.strandh@gmail.com>
2023-03-16 09:29:08 +0100

    Define methods on RELATE to create lexical functions.

--- Code/Builder/flet-ast.lisp
+++ Code/Builder/flet-ast.lisp
@@ -3,3 +3,24 @@
 (define-make-node-method :flet ico:flet-ast)
 
 (define-make-node-method :labels ico:labels-ast)
+
+(define-make-node-method :local-function-binding ico:lexical-function-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :name))
+     (left ico:lexical-function-ast)
+     (right ico:function-name-ast)
+     &key)
+  (reinitialize-instance left
+    :name-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :binding))
+     (left ico:flet-or-labels-ast)
+     (right ico:lexical-function-ast)
+     &key)
+  (reinitialize-instance left
+    :lexical-function-asts
+    (append (ico:lexical-function-asts left) right)))
