d518be2db103c717ebfc21313a6faf65bf47a857
Robert Strandh <robert.strandh@gmail.com>
2023-03-16 06:35:59 +0100

    Add another test for TAGBODY.

--- Code/Builder/Test/tagbody.lisp
+++ Code/Builder/Test/tagbody.lisp
@@ -5,6 +5,16 @@
     (assert (equal (convert-ast result)
                    '(ico:tagbody-ast ("segment-asts" nil))))))
 
+(defun test-tagbody-2 ()
+  (let ((result (bld::test '(tagbody a))))
+    (assert (equal (convert-ast result)
+                   '(ico:tagbody-ast
+                     ("segment-asts"
+                      ((ico:tagbody-segment-ast
+                        ("form-asts" nil)
+                        ("tag-ast" (ico:tag-ast :name a))))))))))
+
 (defun test-tagbody ()
   (format *trace-output* "Testing TAGBODY~%")
-  (test-tagbody-1))
+  (test-tagbody-1)
+  (test-tagbody-2))
