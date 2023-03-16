822ab1aea0f56a23e33d010c1e1460bef06a5e10
Robert Strandh <robert.strandh@gmail.com>
2023-03-16 16:52:57 +0100

    Rename AST class to account for MACROLET.

--- Code/AST/flet-ast.lisp
+++ Code/AST/flet-ast.lisp
@@ -3,21 +3,21 @@
 (defclass lexical-function-ast (name-ast-mixin function-definition-ast)
   ())
 
-(defclass flet-or-labels-ast
+(defclass flet-or-labels-or-macrolet-ast
     (declaration-asts-mixin form-asts-mixin ast)
   ((%lexical-function-asts
       :initform '()
       :initarg :lexical-function-asts
       :reader lexical-function-asts)))
 
-(defmethod children append ((ast flet-or-labels-ast))
+(defmethod children append ((ast flet-or-labels-or-macrolet-ast))
   (list (cons "lexical-function-asts" (lexical-function-asts ast))))
 
-(defclass flet-ast (flet-or-labels-ast)
+(defclass flet-ast (flet-or-labels-or-macrolet-ast)
   ())
 
-(defclass labels-ast (flet-or-labels-ast)
+(defclass labels-ast (flet-or-labels-or-macrolet-ast)
   ())
 
-(defclass macrolet-ast (flet-or-labels-ast)
+(defclass macrolet-ast (flet-or-labels-or-macrolet-ast)
   ())
