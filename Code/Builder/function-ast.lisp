24452e5a54747feed5a599b434244d3b0373df4e
Robert Strandh <robert.strandh@gmail.com>
2023-03-16 06:24:27 +0100

    Provide method on RELATE for FUNCTION-NAME-AST.

--- Code/Builder/function-ast.lisp
+++ Code/Builder/function-ast.lisp
@@ -4,6 +4,15 @@
 
 (defmethod abp:relate
     ((builder builder)
+     (relation (eql :name))
+     (left ico:function-ast)
+     (right ico:function-name-ast)
+     &key)
+  (reinitialize-instance left
+    :name-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
      (relation (eql :lambda))
      (left ico:function-ast)
      (right ico:function-definition-ast)
