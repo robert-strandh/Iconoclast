f3da4aaeeb287fdb352cd1bfeb68621397d9f99a
Robert Strandh <robert.strandh@gmail.com>
2023-03-16 09:29:35 +0100

    Add another test for FLET.

--- Code/Builder/Test/flet.lisp
+++ Code/Builder/Test/flet.lisp
@@ -8,6 +8,21 @@
                      ("form-asts" nil)
                      ("lexical-function-asts" nil))))))
 
+(defun test-flet-2 ()
+  (let ((result (bld::test '(flet ((f ()))))))
+    (assert (equal (convert-ast result)
+                   '(ico:flet-ast
+                     ("declaration-asts" nil)
+                     ("form-asts" nil)
+                     ("lexical-function-asts"
+                      (ico:lexical-function-ast
+                       ("declaration-asts" nil)
+                       ("documentation-ast" nil)
+                       ("form-asts" nil)
+                       ("name-ast"
+                        (ico:function-name-ast :name f)))))))))
+
 (defun test-flet ()
   (format *trace-output* "Testing FLET~%")
-  (test-flet-1))
+  (test-flet-1)
+  (test-flet-2))
