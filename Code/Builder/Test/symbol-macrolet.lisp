0957dee40ae9094a91b70369d47ed87d3986cce8
Robert Strandh <robert.strandh@gmail.com>
2023-03-17 15:04:43 +0100

    Add a second test for SYMBOL-MACROLET.

--- Code/Builder/Test/symbol-macrolet.lisp
+++ Code/Builder/Test/symbol-macrolet.lisp
@@ -8,6 +8,18 @@
                      ("form-asts" nil)
                      ("symbol-expansion-asts" nil))))))
 
+(defun test-symbol-macrolet-2 ()
+  (let ((result (bld::test '(symbol-macrolet ((f 234))))))
+    (assert (equal (convert-ast result)
+                   '(ico:symbol-macrolet-ast
+                     ("declaration-asts" nil)
+                     ("form-asts" nil)
+                     ("symbol-expansion-asts"
+                      ((ico:symbol-expansion-ast
+                        ("expansion-ast" (bld:unparsed-form-ast :form 234))
+                        ("symbol-ast" (ico:variable-ast :name f))))))))))
+
 (defun test-symbol-macrolet ()
   (format *trace-output* "Testing SYMBOL-MACROLET~%")
-  (test-symbol-macrolet-1))
+  (test-symbol-macrolet-1)
+  (test-symbol-macrolet-2))
