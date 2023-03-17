c98a38ff53c8d582e539505c70333775e2a65306
Robert Strandh <robert.strandh@gmail.com>
2023-03-17 15:37:41 +0100

    Add case SPECIAL for DECLARATION-KIND.

--- Code/Builder/declare-ast.lisp
+++ Code/Builder/declare-ast.lisp
@@ -14,6 +14,7 @@
         (inline 'ico:inline-ast)
         (notinline 'ico:notinline-ast)
         (optimize 'ico:optimize-ast)
+        (special 'ico:special-ast)
         #+(or)(declaration 'ico:declaration-ast))
     :origin source))
 
