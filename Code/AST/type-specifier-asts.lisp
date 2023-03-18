95cf1a1c967cbfa779f137cdf6b7b9f6954f0c51
Robert Strandh <robert.strandh@gmail.com>
2023-03-18 10:37:22 +0100

    Add classes, readers and methods for COMPOUND-TYPE-SPECIFIER-AST.

--- Code/AST/type-specifier-asts.lisp
+++ Code/AST/type-specifier-asts.lisp
@@ -3,5 +3,27 @@
 (defclass type-name-ast (name-mixin ast)
   ())
 
-(defclass atomic-type-specifier-ast (name-ast-mixin ast)
+(defclass type-specifier-ast (name-ast-mixin ast)
   ())
+
+(defclass atomic-type-specifier-ast (type-specifier-ast)
+  ())
+
+(defclass subsidiary-item-ast (ast)
+  ((%value
+    :initarg :value
+    :reader value)))
+
+(defclass compound-type-specifier-ast (type-specifier-ast)
+  ((%subsidiary-item-asts
+    :initform '()
+    :initarg :subsidiary-item-asts
+    :reader subsidiary-item-asts)
+   (%atomic-type-specifier-ast
+    :initform nil
+    :initarg :atomic-type-specifier-ast
+    :reader atomic-type-specifier-ast)))
+
+(defmethod children append ((ast compound-type-specifier-ast))
+  (list (cons "subsidiary-item-asts" (subsidiary-item-asts ast))
+        (cons "atomic-type-specifier-ast" (atomic-type-specifier-ast ast))))
