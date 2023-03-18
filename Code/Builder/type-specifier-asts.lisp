faf460a0db5c98005e3659b0c6948b85c1543e13
Robert Strandh <robert.strandh@gmail.com>
2023-03-18 10:38:30 +0100

    Add methods on MAKE-NODE and RELATE for COMPOUND-TYPE-SPECIFIER-AST.

--- Code/Builder/type-specifier-asts.lisp
+++ Code/Builder/type-specifier-asts.lisp
@@ -18,3 +18,32 @@
      &key)
   (reinitialize-instance left
     :name-ast right))
+
+(defmethod abp:make-node
+    ((builder builder)
+     (kind (eql :subsidiary-item))
+     &key source value)
+  (make-instance 'ico:subsidiary-item-ast
+    :origin source
+    :value value))
+
+(define-make-node-method :compound-type-specifier ico:compound-type-specifier-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :name))
+     (left ico:compound-type-specifier-ast)
+     (right ico:type-name-ast)
+     &key)
+  (reinitialize-instance left
+    :name-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :argument))
+     (left ico:compound-type-specifier-ast)
+     (right ico:subsidiary-item-ast)
+     &key)
+  (reinitialize-instance left
+    :subsidiary-item-asts
+    (append (ico:subsidiary-item-asts left) (list right))))
