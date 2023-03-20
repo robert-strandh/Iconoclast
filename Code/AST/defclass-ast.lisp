63dd3f99e6f5a452f3fc2c64acb4f0caa8e554b9
Robert Strandh <robert.strandh@gmail.com>
2023-03-20 10:24:18 +0100

    Define slot in DEFCLASS-AST holding a list of SLOT-SPECIFIER-AST.

--- Code/AST/defclass-ast.lisp
+++ Code/AST/defclass-ast.lisp
@@ -3,5 +3,10 @@
 (defclass slot-specifier-ast (name-ast-mixin ast)
   ())
 
+(defgeneric slot-specifier-asts (ast))
+
 (defclass defclass-ast (name-ast-mixin ast)
-  ())
+  ((%slot-specifier-asts
+    :initform '()
+    :initarg :slot-specifier-asts
+    :reader slot-specifier-asts)))
