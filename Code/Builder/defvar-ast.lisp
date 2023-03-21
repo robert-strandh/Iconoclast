2d707df00ea3d5630bb680a06101f7d161ed4739
Robert Strandh <robert.strandh@gmail.com>
2023-03-21 15:32:03 +0100

    New component with methods for DFEVAR and DEFPARAMETER.

---
+++ Code/Builder/defvar-ast.lisp
@@ -0,0 +1,23 @@
+(cl:in-package #:iconoclast-builder)
+
+(define-make-node-method :defvar ico:defvar-ast)
+
+(define-make-node-method :defparameter ico:defparameter-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :name))
+     (left ico:defvar-or-defparameter-ast)
+     (right ico:variable-ast)
+     &key)
+  (reinitialize-instance left
+    :variable-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :initial-value))
+     (left ico:defvar-or-defparameter-ast)
+     (right unparsed-form-ast)
+     &key)
+  (reinitialize-instance left
+    :form-ast right))
