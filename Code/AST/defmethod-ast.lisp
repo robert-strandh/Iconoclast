115806a82534c453db2261b915f7c733b79b0e56
Robert Strandh <robert.strandh@gmail.com>
2023-03-23 07:34:58 +0100

    Define class SPECIALIZED-PARAMETER-AST and slot REQUIRED-AST.

--- Code/AST/defmethod-ast.lisp
+++ Code/AST/defmethod-ast.lisp
@@ -1,7 +1,20 @@
 (cl:in-package #:iconoclast)
 
+(defclass specialized-parameter-ast (name-ast-mixin ast)
+  ((%specializer-ast
+    :initform nil
+    :initarg :specializer-ast
+    :reader specializer-ast)))
+
+(defgeneric required-asts (lambda-list-ast))
+
 (defclass specialized-lambda-list-ast (lambda-list-ast)
-  ())
+  (;; This slot should probably be moved to a superclass or to a mixin
+   ;; class.
+   (%required-asts
+    :initform '()
+    :initarg :required-asts
+    :reader required-asts)))
 
 (defclass defmethod-ast (name-ast-mixin function-definition-ast)
   ())
