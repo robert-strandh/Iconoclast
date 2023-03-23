f15014586619734a83c0f693c37400b2d24aa081
Robert Strandh <robert.strandh@gmail.com>
2023-03-23 07:36:08 +0100

    Add methods for required parameters in specialized lambda lists.

--- Code/Builder/defmethod-ast.lisp
+++ Code/Builder/defmethod-ast.lisp
@@ -1,5 +1,34 @@
 (cl:in-package #:iconoclast-builder)
 
+(define-make-node-method :specialized-parameter ico:specialized-parameter-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :name))
+     (left ico:specialized-parameter-ast)
+     (right ico:variable-ast)
+     &key)
+  (reinitialize-instance left
+    :name-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :specializer))
+     (left ico:specialized-parameter-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left
+    :specializer-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :required))
+     (left ico:specialized-lambda-list-ast)
+     (right ico:specialized-parameter-ast)
+     &key)
+  (reinitialize-instance left
+    :required-asts (append (ico:required-asts left) (list right))))
+
 (define-make-node-method
     :specialized-lambda-list ico:specialized-lambda-list-ast)
 
