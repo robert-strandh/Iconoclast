b9a569716175dba1bc84c767e33bd8840adcda41
Robert Strandh <robert.strandh@gmail.com>
2023-03-23 12:33:10 +0100

    Remove code now in new component for lambda lists.

--- Code/Builder/defmethod-ast.lisp
+++ Code/Builder/defmethod-ast.lisp
@@ -20,18 +20,6 @@
   (reinitialize-instance left
     :specializer-ast right))
 
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :required))
-     (left ico:specialized-lambda-list-ast)
-     (right ico:specialized-parameter-ast)
-     &key)
-  (reinitialize-instance left
-    :required-asts (append (ico:required-asts left) (list right))))
-
-(define-make-node-method
-    :specialized-lambda-list ico:specialized-lambda-list-ast)
-
 (define-make-node-method :defmethod ico:defmethod-ast)
 
 (defmethod abp:relate
