2b2474d8319426f8acb15331aa8034e03ca48072
Robert Strandh <robert.strandh@gmail.com>
2023-03-24 15:50:33 +0100

    Define SETQ-OR-PSETQ-AST as superclass of SETQ-AST and PSETQ-AST.

--- Code/AST/setq-ast.lisp
+++ Code/AST/setq-ast.lisp
@@ -2,11 +2,17 @@
 
 (defgeneric variable-name-asts (ast))
 
-(defclass setq-ast (form-asts-mixin ast)
+(defclass setq-or-psetq-ast (form-asts-mixin ast)
   ((%variable-name-asts
       :initform '()
       :initarg :variable-name-asts
       :reader variable-name-asts)))
 
-(defmethod children append ((ast setq-ast))
+(defmethod children append ((ast setq-or-psetq-ast))
   (list (cons "variable-name-asts" (variable-name-asts ast))))
+
+(defclass setq-ast (setq-or-psetq-ast)
+  ())
+
+(defclass psetq-ast (setq-or-psetq-ast)
+  ())
