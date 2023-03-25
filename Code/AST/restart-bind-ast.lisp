8a31947d3c467454ee6a7bfa37293c8770716f9c
Robert Strandh <robert.strandh@gmail.com>
2023-03-25 15:09:46 +0100

    Define slots for RESTART-BIND-AST and RESTART-BINDING-AST.

--- Code/AST/restart-bind-ast.lisp
+++ Code/AST/restart-bind-ast.lisp
@@ -1,4 +1,14 @@
 (cl:in-package #:iconoclast)
 
+(defclass restart-binding-ast (name-ast-mixin ast)
+  (;; Maybe factor out this slot.  See for instance
+   ;; MULTIPLE-VALUE-CALL-AST.
+   (%function-form-ast
+    :initarg :function-form-ast
+    :reader function-form-ast)))
+
 (defclass restart-bind-ast (form-asts-mixin ast)
-  ())
+  ((%restart-binding-asts
+    :initform '()
+    :initarg :restart-binding-asts
+    :reader restart-binding-asts)))
