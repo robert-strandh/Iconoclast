6caf09f4343ad3c0477f4bcb8b47042e267f4c4c
Robert Strandh <robert.strandh@gmail.com>
2023-03-25 08:19:48 +0100

    Define class RESTART-CLAUSE-AST and add slot to RESTART-CASE-AST.

--- Code/AST/restart-case-ast.lisp
+++ Code/AST/restart-case-ast.lisp
@@ -1,4 +1,11 @@
 (cl:in-package #:iconoclast)
 
-(defclass restart-case-ast (form-ast-mixin ast)
+(defclass restart-clause-ast
+    (lambda-list-ast-mixin name-ast-mixin ast)
   ())
+
+(defclass restart-case-ast (form-ast-mixin ast)
+  ((%clause-asts
+    :initform '()
+    :initarg :clause-asts
+    :reader clause-asts)))
