f4a15890a053e9a750bef9881d5c78e7c60cecca
Robert Strandh <robert.strandh@gmail.com>
2023-03-25 08:21:04 +0100

    Add methods related to RESTART-CASE-AST.

--- Code/Builder/restart-case-ast.lisp
+++ Code/Builder/restart-case-ast.lisp
@@ -1,6 +1,16 @@
 (cl:in-package #:iconoclast-builder)
 
+(define-make-node-method :restart-clause ico:restart-clause-ast)
+
 (define-make-node-method :restart-case ico:restart-case-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :name))
+     (left ico:restart-clause-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :name-ast right))
 
 (defmethod abp:relate
     ((builder builder)
@@ -9,3 +19,12 @@
      (right t)
      &key)
   (reinitialize-instance left :form-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :clause))
+     (left ico:restart-case-ast)
+     (right ico:restart-clause-ast)
+     &key)
+  (reinitialize-instance left
+    :clause-asts (append (ico:clause-asts left) (list right))))
