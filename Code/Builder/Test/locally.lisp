0b9fa484d7aebf88269e7aa3e302b61e221678da
Robert Strandh <robert.strandh@gmail.com>
2023-03-26 09:52:13 +0200

    Use new macro to test LOCALLY and SETQ.

--- Code/Builder/Test/locally.lisp
+++ Code/Builder/Test/locally.lisp
@@ -3,29 +3,29 @@
 ;;; Test that (LOCALLY) generates a LOCALLY-AST with an empty
 ;;; list of declarations and an empty list of forms.
 (defun test-locally-1 ()
-  (let ((result (bld::test '(locally))))
-    (assert (equal (convert-ast result)
-                   '(ico:locally-ast
-                     ("declaration-asts" nil)
-                     ("form-asts" nil))))))
+  (run-test
+   '(locally)
+   '(ico:locally-ast
+     ("declaration-asts" nil)
+     ("form-asts" nil))))
 
 (defun test-locally-2 ()
-  (let ((result (bld::test '(locally 234))))
-    (assert (equal (convert-ast result)
-                   '(ico:locally-ast
-                     ("declaration-asts" nil)
-                     ("form-asts"
-                      ((bld:unparsed-form-ast :form 234))))))))
+  (run-test
+   '(locally 234)
+   '(ico:locally-ast
+     ("declaration-asts" nil)
+     ("form-asts"
+      ((bld:unparsed-form-ast :form 234))))))
 
 ;;; Test that the FORM-ASTs appear in the right order.
 (defun test-locally-3 ()
-  (let ((result (bld::test '(locally 234 345))))
-    (assert (equal (convert-ast result)
-                   '(ico:locally-ast
-                     ("declaration-asts" nil)
-                     ("form-asts"
-                      ((bld:unparsed-form-ast :form 234)
-                       (bld:unparsed-form-ast :form 345))))))))
+  (run-test
+   '(locally 234 345)
+   '(ico:locally-ast
+     ("declaration-asts" nil)
+     ("form-asts"
+      ((bld:unparsed-form-ast :form 234)
+       (bld:unparsed-form-ast :form 345))))))
 
 ;;; None of these tests has any declarations in it.  We test for
 ;;; declarations using LOCALLY in a later test when we test the
--- Code/Builder/Test/setq.lisp
+++ Code/Builder/Test/setq.lisp
@@ -1,31 +1,31 @@
 (cl:in-package #:iconoclast-builder-test)
 
 (defun test-setq-1 ()
-  (let ((result (bld::test '(setq))))
-    (assert (equal (convert-ast result)
-                   '(ico:setq-ast
-                     ("form-asts" nil)
-                     ("variable-name-asts" nil))))))
+  (run-test
+      '(setq)
+      '(ico:setq-ast
+        ("form-asts" nil)
+        ("variable-name-asts" nil))))
 
 (defun test-setq-2 ()
-  (let ((result (bld::test '(setq a 234))))
-    (assert (equal (convert-ast result)
-                   '(ico:setq-ast
-                     ("form-asts"
-                      ((bld:unparsed-form-ast :form 234)))
-                     ("variable-name-asts"
-                      ((ico:variable-name-ast :name a))))))))
+  (run-test
+      '(setq a 234)
+      '(ico:setq-ast
+        ("form-asts"
+         ((bld:unparsed-form-ast :form 234)))
+        ("variable-name-asts"
+         ((ico:variable-name-ast :name a))))))
 
 (defun test-setq-3 ()
-  (let ((result (bld::test '(setq a 234 b 345))))
-    (assert (equal (convert-ast result)
-                   '(ico:setq-ast
-                     ("form-asts"
-                      ((bld:unparsed-form-ast :form 234)
-                       (bld:unparsed-form-ast :form 345)))
-                     ("variable-name-asts"
-                      ((ico:variable-name-ast :name a)
-                       (ico:variable-name-ast :name b))))))))
+  (run-test 
+      '(setq a 234 b 345)
+      '(ico:setq-ast
+        ("form-asts"
+         ((bld:unparsed-form-ast :form 234)
+          (bld:unparsed-form-ast :form 345)))
+        ("variable-name-asts"
+         ((ico:variable-name-ast :name a)
+          (ico:variable-name-ast :name b))))))
 
 (defun test-setq ()
   (format *trace-output* "Testing SETQ~%")
