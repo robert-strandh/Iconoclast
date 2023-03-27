5f3e95c0103695f490336cb3e8beaa739ffa6426
Robert Strandh <robert.strandh@gmail.com>
2023-03-27 06:27:39 +0200

    Use new RUN-TEST macro for TAGBODY and GO.

--- Code/Builder/Test/go.lisp
+++ Code/Builder/Test/go.lisp
@@ -1,10 +1,10 @@
 (cl:in-package #:iconoclast-builder-test)
 
 (defun test-go-1 ()
-  (let ((result (bld::test '(go 234))))
-    (assert (equal (convert-ast result)
-                   '(ico:go-ast
-                     ("tag-ast" (ico:tag-ast :name 234)))))))
+  (run-test
+   '(go 234)
+   '(ico:go-ast
+     ("tag-ast" (ico:tag-ast :name 234)))))
 
 (defun test-go ()
   (format *trace-output* "Testing GO~%")
--- Code/Builder/Test/tagbody.lisp
+++ Code/Builder/Test/tagbody.lisp
@@ -1,18 +1,18 @@
 (cl:in-package #:iconoclast-builder-test)
 
 (defun test-tagbody-1 ()
-  (let ((result (bld::test '(tagbody))))
-    (assert (equal (convert-ast result)
-                   '(ico:tagbody-ast ("segment-asts" nil))))))
+  (run-test
+   '(tagbody)
+   '(ico:tagbody-ast ("segment-asts" nil))))
 
 (defun test-tagbody-2 ()
-  (let ((result (bld::test '(tagbody a))))
-    (assert (equal (convert-ast result)
-                   '(ico:tagbody-ast
-                     ("segment-asts"
-                      ((ico:tagbody-segment-ast
-                        ("form-asts" nil)
-                        ("tag-ast" (ico:tag-ast :name a))))))))))
+  (run-test
+   '(tagbody a)
+   '(ico:tagbody-ast
+     ("segment-asts"
+      ((ico:tagbody-segment-ast
+        ("form-asts" nil)
+        ("tag-ast" (ico:tag-ast :name a))))))))
 
 (defun test-tagbody ()
   (format *trace-output* "Testing TAGBODY~%")
