43aa502d20aa278b46d21fe3c86557b10a2d619e
Robert Strandh <robert.strandh@gmail.com>
2023-03-27 07:48:35 +0200

    Use new RUN-TEST macro for MULTIPLE-VALUE-CALL/PROG1.

--- Code/Builder/Test/multiple-value-call.lisp
+++ Code/Builder/Test/multiple-value-call.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:iconoclast-builder-test)
 
 (defun test-multiple-value-call-1 ()
-  (let ((result (bld::test '(multiple-value-call a))))
-    (assert (equal (convert-ast result)
-                   '(ico:multiple-value-call-ast
-                     ("form-asts" nil)
-                     ("function-form-ast"
-                      (bld:unparsed-form-ast :form a)))))))
+  (run-test
+   '(multiple-value-call a)
+   '(ico:multiple-value-call-ast
+     ("form-asts" nil)
+     ("function-form-ast"
+      (bld:unparsed-form-ast :form a)))))
 
 (defun test-multiple-value-call ()
   (format *trace-output* "Testing MULTIPLE-VALUE-CALL~%")
--- Code/Builder/Test/multiple-value-prog1.lisp
+++ Code/Builder/Test/multiple-value-prog1.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:iconoclast-builder-test)
 
 (defun test-multiple-value-prog1-1 ()
-  (let ((result (bld::test '(multiple-value-prog1 a))))
-    (assert (equal (convert-ast result)
-                   '(ico:multiple-value-prog1-ast
-                     ("first-form-ast"
-                      (bld:unparsed-form-ast :form a))
-                     ("form-asts" nil))))))
+  (run-test
+   '(multiple-value-prog1 a)
+   '(ico:multiple-value-prog1-ast
+     ("first-form-ast"
+      (bld:unparsed-form-ast :form a))
+     ("form-asts" nil))))
 
 (defun test-multiple-value-prog1 ()
   (format *trace-output* "Testing MULTIPLE-VALUE-PROG1~%")
