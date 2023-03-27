c53df83c96e7e30ddfa1a8c603c3c8e7c0f37ec6
Robert Strandh <robert.strandh@gmail.com>
2023-03-27 10:15:38 +0200

    Define method on MAKE-NODE for TYPECASE-NORMAL-CLAUSE-AST.

--- Code/Builder/typecase-ast.lisp
+++ Code/Builder/typecase-ast.lisp
@@ -1,5 +1,8 @@
 (cl:in-package #:iconoclast-builder)
 
+(define-make-node-method
+    :typecase-normal-clause ico:typecase-normal-clause-ast)
+
 (define-make-node-method :typecase ico:typecase-ast)
 
 (define-make-node-method :etypecase ico:etypecase-ast)
