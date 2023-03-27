6041fbf89e7b46b8aae4710b80cd435aff05279e
Robert Strandh <robert.strandh@gmail.com>
2023-03-27 06:26:35 +0200

    Use new RUN-TEST macro for FUNCTION and UNWIND-PROTECT.

--- Code/Builder/Test/function.lisp
+++ Code/Builder/Test/function.lisp
@@ -3,20 +3,20 @@
 ;;; Test that (FUNCTION) generates a FUNCTION-AST with an empty
 ;;; list of declarations and an empty list of forms.
 (defun test-function-1 ()
-  (let ((result (bld::test '(function f))))
-    (assert (equal (convert-ast result)
-                   '(ico:function-ast
-                     ("name-ast" (ico:function-name-ast :name f)))))))
+  (run-test
+   '(function f)
+   '(ico:function-ast
+     ("name-ast" (ico:function-name-ast :name f)))))
 
 (defun test-function-2 ()
-  (let ((result (bld::test '(function (lambda ())))))
-    (assert (equal (convert-ast result)
-                   '(ico:function-ast
-                     ("name-ast"
-                      (ico:function-definition-ast
-                       ("declaration-asts" nil)
-                       ("documentation-ast" nil)
-                       ("form-asts" nil))))))))
+  (run-test
+   '(function (lambda ()))
+   '(ico:function-ast
+     ("name-ast"
+      (ico:function-definition-ast
+       ("declaration-asts" nil)
+       ("documentation-ast" nil)
+       ("form-asts" nil))))))
 
 ;;; None of these tests has any declarations in it.  We test for
 ;;; declarations using FUNCTION in a later test when we test the
--- Code/Builder/Test/unwind-protect.lisp
+++ Code/Builder/Test/unwind-protect.lisp
@@ -1,12 +1,12 @@
 (cl:in-package #:iconoclast-builder-test)
 
 (defun test-unwind-protect-1 ()
-  (let ((result (bld::test '(unwind-protect 234))))
-    (assert (equal (convert-ast result)
-                   '(ico:unwind-protect-ast
-                     ("form-asts" nil)
-                     ("protected-form-ast"
-                      (bld:unparsed-form-ast :form 234)))))))
+  (run-test
+   '(unwind-protect 234)
+   '(ico:unwind-protect-ast
+     ("form-asts" nil)
+     ("protected-form-ast"
+      (bld:unparsed-form-ast :form 234)))))
 
 (defun test-unwind-protect ()
   (format *trace-output* "Testing UNWIND-PROTECT~%")
