a46eb556838de30238c974bc40e749858d381188
Robert Strandh <robert.strandh@gmail.com>
2023-03-27 11:14:22 +0200

    Add slot for type specifier to class TYPE-CASE-NORMAL-CLAUSE.

--- Code/AST/typecase-ast.lisp
+++ Code/AST/typecase-ast.lisp
@@ -1,12 +1,14 @@
 (cl:in-package #:iconoclast)
 
-(defclass typecase-clause-ast (ast)
+(defclass typecase-clause-ast (form-asts-mixin ast)
   ())
 
 (defclass typecase-normal-clause-ast (typecase-clause-ast)
-  ())
+  ((%type-specifier-ast
+    :initarg :type-specifier-ast
+    :reader type-specifier-ast)))
 
-(defclass typecase-base-ast (form-ast-mixin ast)
+(defclass typecase-base-ast (clause-asts-mixin form-ast-mixin ast)
   ())
 
 (defclass typecase-ast (typecase-base-ast)
