e58f7ec44f5e60b3d5a6084ddbb25cf209e7db13
Robert Strandh <robert.strandh@gmail.com>
2023-03-27 11:15:32 +0200

    Add method on RELATE for :TYPE and :KEYFORM.

--- Code/Builder/typecase-ast.lisp
+++ Code/Builder/typecase-ast.lisp
@@ -3,6 +3,22 @@
 (define-make-node-method
     :typecase-normal-clause ico:typecase-normal-clause-ast)
 
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :type))
+     (left ico:typecase-normal-clause-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :type-specifier-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :keyform))
+     (left ico:typecase-base-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :form-ast right))
+
 (define-make-node-method :typecase ico:typecase-ast)
 
 (define-make-node-method :etypecase ico:etypecase-ast)
