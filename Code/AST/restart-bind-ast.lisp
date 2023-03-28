c1737abe3c9c42a057eca1ffa02a784ffe80d84f
Robert Strandh <robert.strandh@gmail.com>
2023-03-28 10:12:43 +0200

    Add missing method qualifier.

--- Code/AST/restart-bind-ast.lisp
+++ Code/AST/restart-bind-ast.lisp
@@ -7,7 +7,7 @@
     :initarg :function-form-ast
     :reader function-form-ast)))
 
-(defmethod children ((ast restart-binding-ast))
+(defmethod children append ((ast restart-binding-ast))
   (list (cons "function-form-ast" (function-form-ast ast))))
 
 (defclass restart-bind-ast (form-asts-mixin ast)
