2f81b00338e490b3725c41ae9fd4577146d7a769
Robert Strandh <robert.strandh@gmail.com>
2023-03-29 09:46:23 +0200

    Define method on MAKE-NODE and RELATE, related to CASE.

--- Code/Builder/case-ast.lisp
+++ Code/Builder/case-ast.lisp
@@ -10,3 +10,17 @@
      &key)
   (reinitialize-instance left
     :key-asts (append (ico:key-asts left) (list right))))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :keyform))
+     (left ico:case-or-ccase-or-ecase-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :form-ast right))
+
+(define-make-node-method :case ico:case-ast)
+
+(define-make-node-method :ccase ico:ccase-ast)
+
+(define-make-node-method :ecase ico:ecase-ast)
