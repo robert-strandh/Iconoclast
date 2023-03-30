aa3d755aaf745a2b2f47ab38083ef2b3cd525f95
Robert Strandh <robert.strandh@gmail.com>
2023-03-30 06:04:07 +0200

    Define classes CASE-OTHEWISE-CLAUSE-AST and CASE-CLAUSE-AST.

--- Code/AST/case-ast.lisp
+++ Code/AST/case-ast.lisp
@@ -3,13 +3,19 @@
 ;;; FIXME: How do we distinguish between a clause that
 ;;; has the key (say) A and one that has the key (A)?
 
+(defclass case-clause-ast (form-asts-mixin ast)
+  ())
+
 (defgeneric key-asts (ast))
 
-(defclass case-normal-clause-ast (form-asts-mixin ast)
+(defclass case-normal-clause-ast (case-clause-ast)
   ((%key-asts
       :initform '()
       :initarg :key-asts
       :reader key-asts)))
+
+(defclass case-otherwise-clause-ast (case-clause-ast)
+  ())
 
 (defmethod children append ((ast case-normal-clause-ast))
   (list (cons "key-asts" (key-asts ast))))
