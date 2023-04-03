0e67b83a7fb8aeaa167d5775142419578fc66225
Robert Strandh <robert.strandh@gmail.com>
2023-04-03 17:50:04 +0200

    Define subclasses of BLOCK-NAME-AST for definition and reference.

--- Code/AST/block-ast.lisp
+++ Code/AST/block-ast.lisp
@@ -3,6 +3,26 @@
 (defclass block-name-ast (name-ast)
   ())
 
+(defgeneric block-name-reference-asts (block-name-definition-ast))
+
+;;; We do not define a method on CHILDREN for this AST because that
+;;; would create a cycle in the AST graph.
+(defclass block-name-definition-ast (block-name-ast)
+  ((%block-name-reference-asts
+    :initform '()
+    :initarg :block-name-reference-asts
+    :reader block-name-reference-asts)))
+
+(defgeneric block-name-definition-ast (block-name-reference-ast))
+
+;;; We do not define a method on CHILDREN for this AST because that
+;;; would create a cycle in the AST graph.
+(defclass block-name-reference-ast (block-name-ast)
+  ((%block-name-definition-ast
+    :initform '()
+    :initarg :block-name-definition-ast
+    :reader block-name-definition-ast)))
+
 (defclass block-ast (name-ast-mixin form-asts-mixin ast)
   ())
 
