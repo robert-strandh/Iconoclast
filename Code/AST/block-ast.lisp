bb04155bd19aee2b41a70c6fa5f60349cbebfeec
Robert Strandh <robert.strandh@gmail.com>
2023-04-03 18:00:05 +0200

    Redefine RETURN-FROM-AST to have NAME-AST-MIXIN as a superclass.

--- Code/AST/block-ast.lisp
+++ Code/AST/block-ast.lisp
@@ -26,12 +26,6 @@
 (defclass block-ast (name-ast-mixin form-asts-mixin ast)
   ())
 
-(defclass return-from-ast (form-ast-mixin ast)
-  (;; This slot contains the BLOCK-AST that is being returned from.
-   (%block-ast
-      :initarg :block-ast
-      :reader block-ast))
+(defclass return-from-ast (name-ast-mixin form-ast-mixin ast)
+  ()
   (:default-initargs :form-ast nil))
-
-(defmethod children append ((ast return-from-ast))
-  (list (cons "block-ast" (block-ast ast))))
