8e98c5205b1625ff9e634cb9cfb59f93fbc83b86
Robert Strandh <robert.strandh@gmail.com>
2023-04-04 05:38:30 +0200

    Make FORM-AST-MIXIN a superclass of THROW-AST.

--- Code/AST/catch-ast.lisp
+++ Code/AST/catch-ast.lisp
@@ -5,10 +5,5 @@
 
 (defgeneric result-form-ast (ast))
 
-(defclass throw-ast (tag-ast-mixin ast)
-  ((%result-form-ast
-      :initarg :result-form-ast
-      :reader result-form-ast)))
-
-(defmethod children append ((ast throw-ast))
-  (list (cons "result-form-ast" (result-form-ast ast))))
+(defclass throw-ast (form-ast-mixin tag-ast-mixin ast)
+  ())
