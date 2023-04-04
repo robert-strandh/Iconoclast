b11a6d91f17ef1fca3e68a6bbc973c8c4c887fe9
Robert Strandh <robert.strandh@gmail.com>
2023-04-04 06:01:51 +0200

    Define subclasses of TAG-AST for definition and reference.

--- Code/AST/tag-ast.lisp
+++ Code/AST/tag-ast.lisp
@@ -2,3 +2,18 @@
 
 (defclass tag-ast (name-ast)
   ())
+
+(defgeneric tag-reference-asts (tag-definition-ast))
+
+(defclass tag-definition-ast (tag-ast)
+  ((%tag-reference-asts
+    :initform '()
+    :initarg :tag-reference-asts
+    :reader tag-reference-asts)))
+
+(defgeneric tag-definition-ast (tag-reference-ast))
+
+(defclass tag-reference-ast (tag-ast)
+  ((%tag-definition-ast
+    :initarg :tag-definition-ast
+    :reader tag-definition-ast)))
