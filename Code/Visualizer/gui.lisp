ef764761813fd297a480c5be925edfb2d1bae337
Robert Strandh <robert.strandh@gmail.com>
2023-04-05 16:00:38 +0200

    Define generic function for visualizing an AST.

--- Code/Visualizer/gui.lisp
+++ Code/Visualizer/gui.lisp
@@ -12,8 +12,20 @@
                        (4/5 (clim:scrolling () application))
                        (1/5 (clim:scrolling () interactor))))))
 
+(defgeneric display-ast* (ast pane hpos vpos))
+
+(defmethod display-ast* ((ast ico:progn-ast) pane hpos vpos)
+  (clim:draw-rectangle* pane
+                        hpos vpos
+                        (+ hpos 20) (+ vpos 20)
+                        :filled nil)
+  (loop for child in (ico:form-asts ast)
+        for vpos-delta from 0 by 30
+        do (display-ast* child pane
+                         (+ hpos 30) (+ vpos vpos-delta))))
+
 (defun display-ast (frame pane)
-  (declare (ignore frame pane)))
+  (display-ast* (ast frame) pane 10 10))
 
 (defun visualize (ast &key new-process-p)
   (let ((frame (clim:make-application-frame 'visualizer :ast ast)))
