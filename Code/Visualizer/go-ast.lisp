2bd6d741587f6babdaa941a4a729785c4564948c
Robert Strandh <robert.strandh@gmail.com>
2023-04-06 11:01:55 +0200

    Fix incorrect name of AST.

--- Code/Visualizer/go-ast.lisp
+++ Code/Visualizer/go-ast.lisp
@@ -1,7 +1,7 @@
 (cl:in-package #:iconoclast-visualizer)
 
 (defmethod display-ast* ((ast ico:go-ast) pane hpos vpos)
-  (let* ((name "catch")
+  (let* ((name "go")
          (width (+ (clim:stream-string-width pane name) 10))
          (height 20))
     (draw-ast pane hpos vpos width height name)
