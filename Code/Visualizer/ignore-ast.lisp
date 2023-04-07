cf908ed211a7824eb0f114e6f56133c52fbf7554
Robert Strandh <robert.strandh@gmail.com>
2023-04-07 17:23:46 +0200

    Define method on DISPLAY-AST* for IGNORABLE-AST.

--- Code/Visualizer/ignore-ast.lisp
+++ Code/Visualizer/ignore-ast.lisp
@@ -13,3 +13,17 @@
                                       child-vpos)
                         10)))
       (+ child-vpos height))))
+
+(defmethod display-ast* ((ast ico:ignorable-ast) pane hpos vpos)
+  (let* ((name "ignorable")
+         (width (+ (clim:stream-string-width pane name) 10))
+         (height 20))
+    (draw-ast pane hpos vpos width height name)
+    (let ((child-vpos vpos))
+      (loop for child in (ico:name-asts ast)
+            do (setf child-vpos
+                     (+ (display-ast* child pane
+                                      (+ hpos width 10)
+                                      child-vpos)
+                        10)))
+      (+ child-vpos height))))
