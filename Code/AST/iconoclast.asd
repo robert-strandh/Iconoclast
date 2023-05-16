9542ddb4ba28fc06255fa218ac550984eab0732c
Robert Strandh <robert.strandh@gmail.com>
2023-05-16 11:09:56 +0200

    New component containing definition of UNPARSED-FORM-AST

--- Code/AST/iconoclast.asd
+++ Code/AST/iconoclast.asd
@@ -7,6 +7,7 @@
   ((:file "packages")
    (:file "ast")
    (:file "mixins")
+   (:file "unparsed-ast")
    (:file "application-ast")
    (:file "documentation-ast")
    (:file "literal-ast")
---
+++ Code/Builder/unparsed-ast.lisp
@@ -0,0 +1,19 @@
+(cl:in-package #:iconoclast-builder)
+
+(defmethod abp:make-node
+    ((builder builder)
+     (kind (eql :unparsed))
+     &key source expression context)
+  (case context
+    ((:form :restart-report-string)
+     (make-instance 'ico:unparsed-form-ast
+       :origin source
+       :form expression))
+    ((:place)
+     (make-instance 'ico:place-ast
+       :origin source
+       :place expression))
+    ((:key)
+     (make-instance 'ico:key-ast
+       :key expression))
+    (t (error "Unknown context: ~s" context))))
