e804941a68728b1e0621cd234d00b7c822779b1a
Robert Strandh <robert.strandh@gmail.com>
2023-05-17 16:14:44 +0200

    Add class for METHOD-DESCRIPTION-AST and slot for instances.

--- Code/AST/defgeneric-ast.lisp
+++ Code/AST/defgeneric-ast.lisp
@@ -16,7 +16,11 @@
    (%method-combination-name-and-arguments-ast
     :initform nil
     :initarg :method-combination-name-and-arguments-ast
-    :reader method-combination-name-and-arguments-ast)))
+    :reader method-combination-name-and-arguments-ast)
+   (%method-description-asts
+    :initform '()
+    :initarg :method-description-asts
+    :reader method-description-asts)))
 
 (defmethod children append ((ast defgeneric-ast))
   (list (cons "generic-function-class-ast"
@@ -24,7 +28,9 @@
         (cons "method-class-ast"
               (method-class-ast ast))
         (cons "method-combination-name-and-arguments-ast"
-              (method-combination-name-and-arguments-ast ast))))
+              (method-combination-name-and-arguments-ast ast))
+        (cons "method-description-asts"
+              (method-description-asts ast))))
 
 (defclass method-combination-name-and-arguments-ast
     (name-ast-mixin
@@ -38,3 +44,17 @@
     ((ast method-combination-name-and-arguments-ast))
   (list (cons "method-combination-arguments"
               (method-combination-arguments ast))))
+
+(defclass method-description-ast
+    (lambda-list-ast-mixin
+     declaration-asts-mixin
+     documentation-ast-mixin
+     form-asts-mixin
+     ast)
+  ((%method-qualifier-asts
+    :initform '()
+    :initarg :method-qualifier-asts
+    :reader method-qualifier-asts)))
+
+(defmethod children append ((ast defmethod-ast))
+  (list (cons "method-qualifier-asts" (method-qualifier-asts ast))))
