c7238669817ac15da8a389c31e8eeb0d91da7153
Robert Strandh <robert.strandh@gmail.com>
2023-05-17 15:22:49 +0200

    Add slot for METHOD-CLASS-AST

--- Code/AST/defgeneric-ast.lisp
+++ Code/AST/defgeneric-ast.lisp
@@ -7,8 +7,13 @@
      ast)
   ((%generic-function-class-ast
     :initarg :generic-function-class-ast
-    :reader generic-function-class-ast)))
+    :reader generic-function-class-ast)
+   (%method-class-ast
+    :initarg :method-class-ast
+    :reader method-class-ast)))
 
 (defmethod children append ((ast defgeneric-ast))
   (list (cons "generic-function-class-ast"
-              (generic-function-class-ast ast))))
+              (generic-function-class-ast ast))
+        (cons "method-class-ast"
+              (method-class-ast ast))))
