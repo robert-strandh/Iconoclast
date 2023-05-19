2606d41d85745f236a3df02f76568b53b13b602a
Robert Strandh <robert.strandh@gmail.com>
2023-05-19 15:46:23 +0200

    Add slot for METACLASS-AST.

--- Code/AST/defclass-ast.lisp
+++ Code/AST/defclass-ast.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:iconoclast)
 
+(defgeneric metaclass-ast (defclass-ast))
+
 (defclass defclass-ast
     (documentation-ast-mixin slot-specifier-asts-mixin name-ast-mixin ast)
-  ())
+  ((%metaclass-ast
+    :initform nil
+    :initarg :metaclass-ast
+    :reader metaclass-ast)))
+
+(defmethod children append ((ast defclass-ast))
+  (list (cons "metaclass-ast" (metaclass-ast ast))))
