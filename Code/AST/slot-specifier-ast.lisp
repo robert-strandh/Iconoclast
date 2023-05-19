e50f222e2713e88e2c89699d9beac0abf2abe7db
Robert Strandh <robert.strandh@gmail.com>
2023-05-19 10:50:31 +0200

    Add slot READER-ASTS to SLOT-SPECIFICATION-AST.

--- Code/AST/slot-specifier-ast.lisp
+++ Code/AST/slot-specifier-ast.lisp
@@ -4,6 +4,8 @@
 
 (defgeneric initform-ast (slot-specifier-ast))
 
+(defgeneric reader-asts (slot-specifier-ast))
+
 (defclass slot-specifier-ast (name-ast-mixin ast)
   ((%initarg-asts
     :initform '()
@@ -11,11 +13,16 @@
     :reader initarg-asts)
    (%initform-ast
     :initarg :initform-ast
-    :reader initform-ast)))
+    :reader initform-ast)
+   (%reader-asts
+    :initform '()
+    :initarg :reader-asts
+    :reader reader-asts)))
 
 (defmethod children append ((ast slot-specifier-ast))
   (list (cons "initarg-asts" (initarg-asts ast))
-        (cons "initform-ast" (initform-ast ast))))
+        (cons "initform-ast" (initform-ast ast))
+        (cons "reader-asts" (reader-asts ast))))
 
 (defclass initarg-name-ast (name-mixin ast)
   ())
