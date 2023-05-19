47d27dbefbd44fb5eb6e2a1e5eef74ae9016eed9
Robert Strandh <robert.strandh@gmail.com>
2023-05-19 06:55:18 +0200

    Define slot for SLOT-SPECIFIER-AST containing INITARG-ASTs.

--- Code/AST/slot-specifier-ast.lisp
+++ Code/AST/slot-specifier-ast.lisp
@@ -1,7 +1,15 @@
 (cl:in-package #:iconoclast)
 
+(defgeneric initarg-asts (slot-specifier-ast))
+
 (defclass slot-specifier-ast (name-ast-mixin ast)
-  ())
+  ((%initarg-asts
+    :initform '()
+    :initarg :initarg-asts
+    :reader initarg-asts)))
+
+(defmethod children append ((ast slot-specifier-ast))
+  (list (cons "initarg-asts" (initarg-asts ast))))
 
 (defclass initarg-name-ast (name-mixin ast)
   ())
