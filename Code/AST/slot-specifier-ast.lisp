817ccdd07767207525ac5aa90f467d2731b5ec9e
Robert Strandh <robert.strandh@gmail.com>
2023-05-19 13:35:19 +0200

    Add slot DOCUMENTATION-AST to SLOT-SPECIFICATION-AST.

--- Code/AST/slot-specifier-ast.lisp
+++ Code/AST/slot-specifier-ast.lisp
@@ -4,11 +4,15 @@
 
 (defgeneric initform-ast (slot-specifier-ast))
 
+(defgeneric documentation-ast (slot-specifier-ast))
+
 (defgeneric reader-asts (slot-specifier-ast))
 
 (defgeneric writer-asts (slot-specifier-ast))
 
 (defgeneric accessor-asts (slot-specifier-ast))
+
+(defgeneric allocation (slot-specifier-ast))
 
 (defclass slot-specifier-ast (name-ast-mixin ast)
   ((%initarg-asts
@@ -18,6 +22,10 @@
    (%initform-ast
     :initarg :initform-ast
     :reader initform-ast)
+   (%documentation-ast
+    :initform nil
+    :initarg :documentation-ast
+    :reader documentation-ast)
    (%reader-asts
     :initform '()
     :initarg :reader-asts
@@ -29,10 +37,15 @@
    (%accessor-asts
     :initform '()
     :initarg :accessor-asts
-    :reader accessor-asts)))
+    :reader accessor-asts)
+   (%allocation
+    :initform nil
+    :initarg :allocation
+    :reader allocation)))
 
 (defmethod children append ((ast slot-specifier-ast))
   (list (cons "initarg-asts" (initarg-asts ast))
+        (cons "initform-ast" (initform-ast ast))
         (cons "initform-ast" (initform-ast ast))
         (cons "reader-asts" (reader-asts ast))
         (cons "writer-asts" (writer-asts ast))
