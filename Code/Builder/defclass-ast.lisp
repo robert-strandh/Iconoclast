7e1a702dfe7157ece162acd98be75d972ae9db42
Robert Strandh <robert.strandh@gmail.com>
2023-05-19 16:44:30 +0200

    Define method on RELATE for :DEFAULT-INITARG.

--- Code/Builder/defclass-ast.lisp
+++ Code/Builder/defclass-ast.lisp
@@ -26,3 +26,13 @@
      (right ico:type-name-ast)
      &key)
   (reinitialize-instance left :metaclass-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :default-initarg))
+     (left ico:defclass-ast)
+     (right ico:default-initarg-ast)
+     &key)
+  (reinitialize-instance left
+    :default-initarg-asts
+    (append (ico:default-initarg-asts left) (list right))))
