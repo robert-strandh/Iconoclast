27030a48f5294224630eac306e22d3228ccf1532
Robert Strandh <robert.strandh@gmail.com>
2023-05-19 08:17:58 +0200

    Add slot INITFORM-AST to SLOT-SPECIFIER-AST.

--- Code/AST/slot-specifier-ast.lisp
+++ Code/AST/slot-specifier-ast.lisp
@@ -2,14 +2,20 @@
 
 (defgeneric initarg-asts (slot-specifier-ast))
 
+(defgeneric initform-ast (slot-specifier-ast))
+
 (defclass slot-specifier-ast (name-ast-mixin ast)
   ((%initarg-asts
     :initform '()
     :initarg :initarg-asts
-    :reader initarg-asts)))
+    :reader initarg-asts)
+   (%initform-ast
+    :initarg :initform-ast
+    :reader initform-ast)))
 
 (defmethod children append ((ast slot-specifier-ast))
-  (list (cons "initarg-asts" (initarg-asts ast))))
+  (list (cons "initarg-asts" (initarg-asts ast))
+        (cons "initform-ast" (initform-ast ast))))
 
 (defclass initarg-name-ast (name-mixin ast)
   ())
