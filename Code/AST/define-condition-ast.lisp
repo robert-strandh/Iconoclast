9ec2504b42fcd4e71768ca70a64bebacfe9e5b27
Robert Strandh <robert.strandh@gmail.com>
2023-05-20 10:45:10 +0200

    Add REPORT-AST slot and define class CONDITION-REPORT-AST.

--- Code/AST/define-condition-ast.lisp
+++ Code/AST/define-condition-ast.lisp
@@ -6,4 +6,18 @@
      name-ast-mixin
      default-initarg-asts-mixin
      ast)
-  ())
+  ((%report-ast
+    :initform nil
+    :initarg :report-ast
+    :reader report-ast)))
+
+(defmethod children append ((ast define-condition-ast))
+  (list (cons "report-ast" (report-ast ast))))
+
+(defclass condition-report-ast (ast)
+  ((%argument-ast
+    :initarg :argument-ast
+    :reader argument-ast)))
+
+(defmethod children append ((ast define-condition-ast))
+  (list (cons "argument-ast" (argument-ast ast))))
