74378310d7928a20c7f6f28e876961a848446dc2
Robert Strandh <robert.strandh@gmail.com>
2023-05-21 07:37:26 +0200

    Remove method on RELATE for :SLOT, now defined for mixin class.

--- Code/Builder/defclass-ast.lisp
+++ Code/Builder/defclass-ast.lisp
@@ -4,15 +4,6 @@
 
 (defmethod abp:relate
     ((builder builder)
-     (relation (eql :slot))
-     (left ico:defclass-ast)
-     (right ico:slot-specifier-ast)
-     &key)
-  (reinitialize-instance left
-    :slot-specifier-asts (append (ico:slot-specifier-asts left) (list right))))
-
-(defmethod abp:relate
-    ((builder builder)
      (relation (eql :metaclass))
      (left ico:defclass-ast)
      (right ico:type-name-ast)
