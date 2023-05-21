f66fbde1c1dd8f4132c8d00a14e176457dc34875
Robert Strandh <robert.strandh@gmail.com>
2023-05-21 07:30:01 +0200

    Define method on RELATE for :PARENT-TYPE.

--- Code/Builder/superclass-asts-mixin.lisp
+++ Code/Builder/superclass-asts-mixin.lisp
@@ -1,8 +1,20 @@
 (cl:in-package #:iconoclast-builder)
 
+;;; For DEFCLASS the relation is :SUPERCLASS and for DEFINE-CONDITION
+;;; the relation is :PARENT-TYPE, so we need two different methods.
+
 (defmethod abp:relate
     ((builder builder)
      (relation (eql :superclass))
+     (left ico:superclass-asts-mixin)
+     (right t)
+     &key)
+  (reinitialize-instance left
+    :superclass-asts (append (ico:superclass-asts left) (list right))))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :parent-type))
      (left ico:superclass-asts-mixin)
      (right t)
      &key)
