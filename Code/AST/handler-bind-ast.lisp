b86fc453c7ccd870c6b17bc2f0e9459a03b4a1a8
Robert Strandh <robert.strandh@gmail.com>
2023-05-31 15:39:38 +0200

    Simplify HANDLIER-BIND-AST by making BINDING-ASTS-MIXIN a superclass

--- Code/AST/handler-bind-ast.lisp
+++ Code/AST/handler-bind-ast.lisp
@@ -4,13 +4,5 @@
     (type-specifier-ast-mixin form-ast-mixin ast)
   ())
 
-(defgeneric handler-binding-asts (handler-bind-ast))
-
-(defclass handler-bind-ast (form-asts-mixin ast)
-  ((%handler-binding-asts
-    :initform '()
-    :initarg :handler-binding-asts
-    :reader handler-binding-asts)))
-
-(defmethod children append ((ast handler-binding-ast))
-  (list (cons "handler-binding-asts" (handler-binding-asts ast))))
+(defclass handler-bind-ast (binding-asts-mixin form-asts-mixin ast)
+  ())
