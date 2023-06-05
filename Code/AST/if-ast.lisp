a6fc98719840b3d62c84995d5d34f0df2069ed4e
Robert Strandh <robert.strandh@gmail.com>
2023-06-05 16:20:45 +0200

    Initialize ELSE-AST slot to NIL.

--- Code/AST/if-ast.lisp
+++ Code/AST/if-ast.lisp
@@ -8,10 +8,8 @@
   ((%then-ast
       :initarg :then-ast
       :reader then-ast)
-   ;; When there is no else branch in teh source code, this slot
-   ;; contains a LITERAL- AST with NIL in it, but the origin slot of
-   ;; that AST has NIL in it.
    (%else-ast
+      :initform nil
       :initarg :else-ast
       :reader else-ast)))
 
