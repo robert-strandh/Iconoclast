5f3f18a72d0bfad8b267bc0479e3f416801ed003
Robert Strandh <robert.strandh@gmail.com>
2023-06-07 16:10:04 +0200

    Add missing slots to RESTART-BIND-AST.

--- Code/AST/restart-bind-ast.lisp
+++ Code/AST/restart-bind-ast.lisp
@@ -5,7 +5,19 @@
    ;; MULTIPLE-VALUE-CALL-AST.
    (%function-form-ast
     :initarg :function-form-ast
-    :reader function-form-ast)))
+    :reader function-form-ast)
+   (%interactive-function-ast
+    :initform nil
+    :initarg :interactive-function-ast
+    :reader interactive-function-ast)
+   (%report-function-ast
+    :initform nil
+    :initarg :report-function-ast
+    :reader report-function-ast)
+   (%test-function-ast
+    :initform nil
+    :initarg :test-function-ast
+    :reader test-function-ast)))
 
 (defmethod children append ((ast restart-binding-ast))
   (list (cons "function-form-ast" (function-form-ast ast))))
