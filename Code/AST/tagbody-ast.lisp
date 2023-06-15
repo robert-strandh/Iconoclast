1ba1d7874e740eefa98eff3e907e20025fc2106b
Robert Strandh <robert.strandh@gmail.com>
2023-06-15 18:12:05 +0200

    Extract slot SEGMENT-ASTS to new mixin class.

--- Code/AST/tagbody-ast.lisp
+++ Code/AST/tagbody-ast.lisp
@@ -1,18 +1,4 @@
 (cl:in-package #:iconoclast)
 
-(defgeneric segment-asts (ast))
-
-(defclass tagbody-ast (ast)
-  ((%segment-asts
-      :initform '()
-      :initarg :segment-asts
-      :reader segment-asts)))
-
-(defmethod children append ((ast tagbody-ast))
-  (list (cons "segment-asts" (segment-asts ast))))
-
-(defclass tagbody-segment-ast (form-asts-mixin tag-ast-mixin ast)
-  ())
-
-(defclass go-ast (tag-ast-mixin ast)
+(defclass tagbody-ast (segment-asts-mixin ast)
   ())
