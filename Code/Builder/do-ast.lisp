7dfd41d4226165724e2bb8a8bf8c33e92e155dd4
Robert Strandh <robert.strandh@gmail.com>
2023-06-16 16:49:58 +0200

    Define methods for DO and DO*.

---
+++ Code/Builder/do-ast.lisp
@@ -0,0 +1,62 @@
+(cl:in-package #:iconoclast-builder)
+
+(define-make-node-method
+    :do-iteration-variable ico:do-iteration-variable-ast)
+
+(define-make-node-method :do ico:do-ast)
+
+(define-make-node-method :do* ico:do*-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :variable))
+     (left ico:do-iteration-variable-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :variable-name-ast right))
+
+;;; FIXME: :INIT is a back choice for the relation.
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :init))
+     (left ico:do-iteration-variable-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :init-form-ast right))
+
+;;; FIXME: :STEP is a back choice for the relation.
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :step))
+     (left ico:do-iteration-variable-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :step-form-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :variable))
+     (left ico:do-do*-ast)
+     (right t)
+     &key)
+  (reinitialize-instance
+      left :do-iteration-variable-asts
+      (append (ico:do-iteration-variable-asts left) (list right))))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :end-test))
+     (left ico:do-do*-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :end-test-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :result))
+     (left ico:do-do*-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :result-ast right))
