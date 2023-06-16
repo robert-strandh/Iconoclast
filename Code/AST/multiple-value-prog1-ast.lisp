2b972db05ba9ff474b89f3c31145050bab781f6c
Robert Strandh <robert.strandh@gmail.com>
2023-06-16 05:57:59 +0200

    Use new class FIRSTS-FORM-AST-MIXIN as a superclass.

--- Code/AST/multiple-value-prog1-ast.lisp
+++ Code/AST/multiple-value-prog1-ast.lisp
@@ -1,11 +1,5 @@
 (cl:in-package #:iconoclast)
 
-(defgeneric first-form-ast (ast))
-
-(defclass multiple-value-prog1-ast (form-asts-mixin ast)
-  ((%first-form-ast
-      :initarg :first-form-ast
-      :reader first-form-ast)))
-
-(defmethod children append ((ast multiple-value-prog1-ast))
-  (list (cons "first-form-ast" (first-form-ast ast))))
+(defclass multiple-value-prog1-ast
+    (first-form-ast-mixin form-asts-mixin ast)
+  ())
