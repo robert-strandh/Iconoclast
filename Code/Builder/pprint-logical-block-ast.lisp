7892c67d76b308d56e49f7cae57fd4665903afb2
Robert Strandh <robert.strandh@gmail.com>
2023-06-17 14:36:10 +0200

    New component containing methods for PPRINT-LOGICAL-BLOCK-AST.

---
+++ Code/Builder/pprint-logical-block-ast.lisp
@@ -0,0 +1,43 @@
+(cl:in-package #:iconoclast-builder)
+
+(define-make-node-method :pprint-logical-block ico:pprint-logical-block-ast)
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :stream-symbol))
+     (left ico:pprint-logical-block-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :stream-symbol-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :object))
+     (left ico:pprint-logical-block-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :object-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :prefix))
+     (left ico:pprint-logical-block-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :prefix-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :per-line-prefix))
+     (left ico:pprint-logical-block-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :per-line-prefix-ast right))
+
+(defmethod abp:relate
+    ((builder builder)
+     (relation (eql :suffix))
+     (left ico:pprint-logical-block-ast)
+     (right t)
+     &key)
+  (reinitialize-instance left :suffix-ast right))
