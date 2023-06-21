f6a8421c2d3c3c6ef88b46cab482bf2c6b78c022
Robert Strandh <robert.strandh@gmail.com>
2023-06-21 16:56:03 +0200

    Use new macro DEFINE-RELATIONS in WITH-INPUT-FROM-STRING.

--- Code/Builder/with-input-from-string-ast.lisp
+++ Code/Builder/with-input-from-string-ast.lisp
@@ -3,42 +3,9 @@
 (define-make-node-method
     :with-input-from-string ico:with-input-from-string-ast)
 
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :var))
-     (left ico:with-input-from-string-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left :variable-name-ast right))
-
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :string))
-     (left ico:with-input-from-string-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left :string-ast right))
-
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :index))
-     (left ico:with-input-from-string-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left :index-ast right))
-
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :start))
-     (left ico:with-input-from-string-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left :start-ast right))
-
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :end))
-     (left ico:with-input-from-string-ast)
-     (right t)
-     &key)
-  (reinitialize-instance left :end-ast right))
+(define-relations ico:with-input-from-string-ast
+  ((:var t ico:variable-name-ast)
+   (:string t ico:string-ast)
+   (:index t ico:index-ast)
+   (:start t ico:start-ast)
+   (:end t ico:end-ast)))
