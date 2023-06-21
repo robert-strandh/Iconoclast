9a45e1be1390072a81a048591e4bd5727fd0c526
Robert Strandh <robert.strandh@gmail.com>
2023-06-21 15:30:44 +0200

    Use new macro to define WITH-INPUT-FROM-STRING-AST.

--- Code/AST/with-input-from-string-ast.lisp
+++ Code/AST/with-input-from-string-ast.lisp
@@ -1,36 +1,11 @@
 (cl:in-package #:iconoclast)
 
-(defgeneric string-ast (ast))
-
-(defgeneric index-ast (ast))
-
-(defgeneric start-ast (ast))
-
-(defgeneric end-ast (ast))
-
-(defclass with-input-from-string-ast
+(define-ast-class with-input-from-string-ast
     (variable-name-ast-mixin
      declaration-asts-mixin
      form-asts-mixin
      ast)
-  ((%string-ast
-    :initarg :string-ast
-    :reader string-ast)
-   (%index-ast
-    :initform nil
-    :initarg :index-ast
-    :reader index-ast)
-   (%start-ast
-    :initform nil
-    :initarg :start-ast
-    :reader start-ast)
-   (%end-ast
-    :initform nil
-    :initarg :end-ast
-    :reader end-ast)))
-
-(defmethod children append ((ast with-input-from-string-ast))
-  (list (cons "string-ats" (string-ast ast))
-        (cons "index-ats" (index-ast ast))
-        (cons "start-ats" (start-ast ast))
-        (cons "end-ats" (end-ast ast))))
+  ((1 string-ast)
+   (? index-ast)
+   (? start-ast)
+   (? end-ast)))
