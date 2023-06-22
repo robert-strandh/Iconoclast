1c7116b2eceda7c9b79f365911b81a4ed0940360
Robert Strandh <robert.strandh@gmail.com>
2023-06-22 11:19:13 +0200

    Fix incorrect syntax for DEFINE-AST-CLASS.

--- Code/AST/defpackage-ast.lisp
+++ Code/AST/defpackage-ast.lisp
@@ -1,9 +1,7 @@
 (cl:in-package #:iconoclast)
 
 (define-ast-class import-from-ast (name-asts-mixin ast)
-  ((%package-name-ast
-    :initarg :package-name-ast
-    :reader package-name-ast)))
+  ((1 package-name-ast)))
 
 (define-ast-class defpackage-ast (name-ast-mixin documentation-ast-mixin ast)
   ((* nickname-asts)
