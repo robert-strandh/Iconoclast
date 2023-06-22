c8b92f32e8f92143368e8a5ecc41fb6a41fa288f
Robert Strandh <robert.strandh@gmail.com>
2023-06-22 11:21:51 +0200

    Remove obsolete calls to ABP:NODE-KIND.

--- Code/Builder/name-ast.lisp
+++ Code/Builder/name-ast.lisp
@@ -2,21 +2,9 @@
 
 (define-make-node-method :variable-name ico:variable-name-ast)
 
-(defmethod abp:node-kind
-    ((builder builder) (node ico:variable-name-ast))
-  :variable-name)
-
 (define-make-node-method :type-name ico:type-name-ast)
 
-(defmethod abp:node-kind
-    ((builder builder) (node ico:type-name-ast))
-  :type-name)
-
 (define-make-node-method :function-name ico:function-name-ast)
-
-(defmethod abp:node-kind
-    ((builder builder) (node ico:function-name-ast))
-  :function-name)
 
 (defmethod abp:node-initargs
     ((builder builder)
