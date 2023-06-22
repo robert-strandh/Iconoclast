7ea14f5c1dd67106f577aac8acc42a3b1e577cb6
Robert Strandh <robert.strandh@gmail.com>
2023-06-22 11:19:40 +0200

    Fix incorrect package marker.

--- Code/Builder/application-ast.lisp
+++ Code/Builder/application-ast.lisp
@@ -3,5 +3,5 @@
 (define-make-node-method :application ico:application-ast)
 
 (define-relations ico:application-ast
-  ((:function t ico-function-ast)
-   (:argument t ico-argument-asts)))
+  ((:function t ico:function-ast)
+   (:argument t ico:argument-asts)))
