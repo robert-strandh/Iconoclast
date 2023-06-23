aabd44b8d0165febe871ec98e58cd0c6de5b3a6e
Robert Strandh <robert.strandh@gmail.com>
2023-06-23 08:07:06 +0200

    Make VARIABLE-NAME-AST-MIXIN a superclass of VARIABLE-BINDING-AST.

--- Code/AST/let-ast.lisp
+++ Code/AST/let-ast.lisp
@@ -2,8 +2,9 @@
 
 (defgeneric variable-name-ast (ast))
 
-(define-ast-class variable-binding-ast (form-ast-mixin ast)
-  ((1 variable-name-ast)))
+(define-ast-class variable-binding-ast
+    (form-ast-mixin variable-name-ast-mixin ast)
+  ())
 
 (define-ast-class let-or-let*-ast
     (binding-asts-mixin declaration-asts-mixin form-asts-mixin ast)
