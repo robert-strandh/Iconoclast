32c69fdd2928a6f0822eee5988a1e2471dc11495
Robert Strandh <robert.strandh@gmail.com>
2023-06-29 12:04:09 +0200

    Add FIXME comment.

--- Code/Builder/function-ast.lisp
+++ Code/Builder/function-ast.lisp
@@ -2,6 +2,9 @@
 
 (define-make-node-method :function ico:function-ast)
 
+;;; FIXME: We hope that the s-expression-syntax library
+;;; will have a single relation here.
+
 (define-relations ico:function-ast
   ((:name ico:function-name-ast ico:name-ast)
    (:lambda t ico:name-ast)))
