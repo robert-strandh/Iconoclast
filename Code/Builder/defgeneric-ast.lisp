ac03b2182e49450e3357010491662c42b7311aa6
Robert Strandh <robert.strandh@gmail.com>
2023-06-29 12:04:48 +0200

    Comment out code for methods for now.

--- Code/Builder/defgeneric-ast.lisp
+++ Code/Builder/defgeneric-ast.lisp
@@ -9,35 +9,39 @@
 (define-relations ico:defgeneric-ast
   ((:method-class t ico:method-class-ast)))
 
-(define-make-node-method :method-combination-name ico:name-ast)
+;;; The following code doesn't really work, and we expect an overhaul
+;;; of the method-combination option for DEFGENERIC anyway, so it is
+;;; commented out.
 
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :method-combination))
-     (left ico:defgeneric-ast)
-     (right ico:name-ast)
-     &key)
-  (reinitialize-instance
-      left
-    :method-combination-name-and-arguments-ast
-    (make-instance 'ico:method-combination-name-and-arguments-ast
-      :name-ast right)))
+;; (define-make-node-method :method-combination-name ico:name-ast)
 
-(defmethod abp:relate
-    ((builder builder)
-     (relation (eql :method-combination-argument))
-     (left ico:defgeneric-ast)
-     (right t)
-     &key)
-  (reinitialize-instance
-      (ico:method-combination-name-and-arguments-ast left)
-    :method-combination-arguments
-    (append (ico:method-combination-arguments
-             (ico:method-combination-name-and-arguments-ast left))
-     (list right)))
-  left)
+;; (defmethod abp:relate
+;;     ((builder builder)
+;;      (relation (eql :method-combination))
+;;      (left ico:defgeneric-ast)
+;;      (right ico:name-ast)
+;;      &key)
+;;   (reinitialize-instance
+;;       left
+;;     :method-combination-name-and-arguments-ast
+;;     (make-instance 'ico:method-combination-name-and-arguments-ast
+;;       :name-ast right)))
 
-(define-make-node-method :method-description ico:method-description-ast)
+;; (defmethod abp:relate
+;;     ((builder builder)
+;;      (relation (eql :method-combination-argument))
+;;      (left ico:defgeneric-ast)
+;;      (right t)
+;;      &key)
+;;   (reinitialize-instance
+;;       (ico:method-combination-name-and-arguments-ast left)
+;;     :method-combination-arguments
+;;     (append (ico:method-combination-arguments
+;;              (ico:method-combination-name-and-arguments-ast left))
+;;      (list right)))
+;;   left)
 
-(define-relations ico:defgeneric-ast
-  ((:method ico:method-description-ast ico:method-description-asts)))
+;; (define-make-node-method :method-description ico:method-description-ast)
+
+;; (define-relations ico:defgeneric-ast
+;;   ((:method ico:method-description-ast ico:method-description-asts)))
