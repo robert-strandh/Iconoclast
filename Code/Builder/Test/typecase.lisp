a51914076a65a410839ab011c32ddb11eca0b919
Robert Strandh <robert.strandh@gmail.com>
2023-06-30 07:48:03 +0200

    Fix incorrect test cases.

--- Code/Builder/Test/typecase.lisp
+++ Code/Builder/Test/typecase.lisp
@@ -4,34 +4,34 @@
 
 (define-test typecase-no-clauses
   :parent typecase
-  (compare-parse-and-unparse '(typecase)))
+  (compare-parse-and-unparse '(typecase x)))
 
 (define-test typecase-one-clause-no-form
   :parent typecase
-  (compare-parse-and-unparse '(typecase (t))))
+  (compare-parse-and-unparse '(typecase x (t))))
 
 (define-test typecase-one-clause-one-form
   :parent typecase
-  (compare-parse-and-unparse '(typecase (t 234))))
+  (compare-parse-and-unparse '(typecase x (t 234))))
 
 (define-test typecase-one-clause-two-forms
   :parent typecase
-  (compare-parse-and-unparse '(typecase (t 234 345))))
+  (compare-parse-and-unparse '(typecase x (t 234 345))))
 
 (define-test typecase-two-clauses
   :parent typecase
-  (compare-parse-and-unparse '(typecase (fixnum 11) (t 234 345))))
+  (compare-parse-and-unparse '(typecase x (fixnum 11) (t 234 345))))
 
 (define-test typecase-otherwise-clause-no-form
   :parent typecase
-  (compare-parse-and-unparse '(typecase (otherwise))))
+  (compare-parse-and-unparse '(typecase x (otherwise))))
 
 (define-test typecase-otherwise-clause-with-form
   :parent typecase
-  (compare-parse-and-unparse '(typecase (otherwise x))))
+  (compare-parse-and-unparse '(typecase x (otherwise x))))
 
 (define-test typecase-normal-and-otherwise-clause-with-form
   :parent typecase
-  (compare-parse-and-unparse '(typecase (t a) (otherwise x))))
+  (compare-parse-and-unparse '(typecase x (t a) (otherwise x))))
 
 
