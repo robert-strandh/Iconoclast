d8782b59cd55fa8a1d6b7cf75aadfd5c53e6c854
Robert Strandh <robert.strandh@gmail.com>
2023-06-30 07:45:30 +0200

    Define a specific slot for the variable.  Remove mixin superclass.

--- Code/AST/with-output-to-string-ast.lisp
+++ Code/AST/with-output-to-string-ast.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:iconoclast)
 
 (define-ast-class with-output-to-string-ast
-    (variable-name-ast-mixin
-     declaration-asts-mixin
+    (declaration-asts-mixin
      form-asts-mixin
      ast)
-  ((1 string-ast)
+  ((1 var-ast)
+   (? string-ast)
    (? element-type-ast)))
--- Code/Builder/with-output-to-string-ast.lisp
+++ Code/Builder/with-output-to-string-ast.lisp
@@ -4,6 +4,6 @@
     :with-output-to-string ico:with-output-to-string-ast)
 
 (define-relations ico:with-output-to-string-ast
-  ((:var t ico:variable-name-ast)
+  ((:var t ico:var-ast)
    (:string t ico:string-ast)
    (:element-type t ico:element-type-ast)))
