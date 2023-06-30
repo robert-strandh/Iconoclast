15f01f98fd44b279dd394df47c73e1d39afc1d9f
Robert Strandh <robert.strandh@gmail.com>
2023-06-30 08:31:27 +0200

    Define specific slot for VALUES-FORM-AST.  Remove mixin superclass.

--- Code/AST/multiple-value-prog1-ast.lisp
+++ Code/AST/multiple-value-prog1-ast.lisp
@@ -1,5 +1,4 @@
 (cl:in-package #:iconoclast)
 
-(define-ast-class multiple-value-prog1-ast
-    (first-form-ast-mixin form-asts-mixin ast)
-  ())
+(define-ast-class multiple-value-prog1-ast (form-asts-mixin ast)
+  ((1 values-form-ast)))
--- Code/Builder/multiple-value-prog1-ast.lisp
+++ Code/Builder/multiple-value-prog1-ast.lisp
@@ -3,4 +3,4 @@
 (define-make-node-method :multiple-value-prog1 ico:multiple-value-prog1-ast)
 
 (define-relations ico:multiple-value-prog1-ast
-  ((:values-form t ico:first-form-ast)))
+  ((:values-form t ico:values-form-ast)))
