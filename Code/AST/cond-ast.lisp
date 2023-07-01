792e4c93f4c55d042fd48bab21138df6d8b277aa
Robert Strandh <robert.strandh@gmail.com>
2023-07-01 16:30:03 +0200

    COND-AST now has TEST-AST-MIXIN as a superclass.

--- Code/AST/cond-ast.lisp
+++ Code/AST/cond-ast.lisp
@@ -1,16 +1,7 @@
 (cl:in-package #:iconoclast)
 
-;;; FIXME: We thought it was a good idea to have TEST-AST-MIXIN as a
-;;; superclass here, but TEST-AST-MIXIN is used in the IF-AST and then
-;;; the relation between the IF-AST and the test is called :TEST,
-;;; However a similar relation between COND-CLAUSE-AST and the test is
-;;; called :TEST-FORM, so we can't currently use TEST-AST-MIXIN here.
-
-;; (define-ast-class cond-clause-ast (test-ast-mixin form-asts-mixin ast)
-;;   ())
-
-(define-ast-class cond-clause-ast (form-asts-mixin ast)
-  ((1 test-form-ast)))
+(define-ast-class cond-clause-ast (test-ast-mixin form-asts-mixin ast)
+  ())
 
 (define-ast-class cond-ast (clause-asts-mixin ast)
   ())
--- Code/Builder/cond-ast.lisp
+++ Code/Builder/cond-ast.lisp
@@ -3,6 +3,6 @@
 (define-make-node-method :cond-clause ico:cond-clause-ast)
 
 (define-relations ico:cond-clause-ast
-  ((:test-form t ico:test-form-ast)))
+  ())
 
 (define-make-node-method :cond ico:cond-ast)
