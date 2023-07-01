e95383af46c2f8fac4719b56c43bc45bbb01e61f
Robert Strandh <robert.strandh@gmail.com>
2023-07-01 18:06:34 +0200

    Fix incorrect superclasses and incorrect cardinalities.

--- Code/AST/pprint-logical-block-ast.lisp
+++ Code/AST/pprint-logical-block-ast.lisp
@@ -1,12 +1,11 @@
 (cl:in-package #:iconoclast)
 
 (define-ast-class pprint-logical-block-ast
-    (variable-name-ast-mixin
-     object-ast-mixin
+    (object-ast-mixin
      declaration-asts-mixin
      form-asts-mixin
      ast)
   ((1 stream-symbol-ast)
-   (1 prefix-ast)
-   (1 per-line-prefix-ast)
-   (1 suffix-ast)))
+   (? prefix-ast)
+   (? per-line-prefix-ast)
+   (? suffix-ast)))
