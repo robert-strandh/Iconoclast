48a9a3a1dd0d1075f1778af574770a5d639ae3bb
Robert Strandh <robert.strandh@gmail.com>
2023-07-02 16:13:19 +0200

    Define optional slot TAG-AST and do not inherit from mixin class.

--- Code/AST/segment-asts-mixin.lisp
+++ Code/AST/segment-asts-mixin.lisp
@@ -3,8 +3,9 @@
 (define-ast-class segment-asts-mixin ()
   ((* segment-asts)))
 
-(define-ast-class tagbody-segment-ast (tag-ast-mixin ast)
-  ((* statement-asts)))
+(define-ast-class tagbody-segment-ast (ast)
+  ((? tag-ast)
+   (* statement-asts)))
 
 (define-ast-class go-ast (tag-ast-mixin ast)
   ())
