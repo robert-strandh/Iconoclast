fc17dce0a01284cfcf35bace836c559a1046f607
Robert Strandh <robert.strandh@gmail.com>
2023-07-02 07:50:26 +0200

    Fix incorrect test cases.

--- Code/Builder/Test/defpackage.lisp
+++ Code/Builder/Test/defpackage.lisp
@@ -8,53 +8,67 @@
 
 (define-test defpackage-only-name-symbol
   :parent defpackage
-  (compare-parse-and-unparse '(defpackage hello)))
+  (is #'equal
+      '(defpackage "HELLO")
+      (parse-and-unparse '(defpackage hello))))
 
 (define-test defpackage-only-name-character
   :parent defpackage
-  (compare-parse-and-unparse '(defpackage #\a)))
+  (is #'equal
+      '(defpackage "a")
+      (parse-and-unparse '(defpackage #\a))))
 
 (define-test defpackage-nicknames
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:nicknames a b))))
+  (is #'equal
+      '(defpackage "HELLO" (:nicknames "A" "B"))
+      (parse-and-unparse '(defpackage hello (:nicknames a b)))))
 
 (define-test defpackage-documentation
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:documentation "hello"))))
+  (is #'equal
+      '(defpackage "HELLO" (:documentation "hello"))
+      (parse-and-unparse '(defpackage hello (:documentation "hello")))))
 
 (define-test defpackage-use
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:use x y))))
+  (is #'equal
+      '(defpackage "HELLO" (:use "X" "Y"))
+      (parse-and-unparse '(defpackage hello (:use x y)))))
 
 (define-test defpackage-shadown
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:shadow x y))))
+  (is #'equal
+      '(defpackage "HELLO" (:shadow "X" "Y"))
+      (parse-and-unparse '(defpackage hello (:shadow x y)))))
 
 (define-test defpackage-import-from
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:import-from a b c))))
+  (is #'equal
+      '(defpackage "HELLO" (:import-from "A" "B" "C"))
+      (parse-and-unparse '(defpackage hello (:import-from a b c)))))
 
 (define-test defpackage-shadowing-import
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:shadowing-import-from a b c))))
+  (is #'equal
+      '(defpackage "HELLO" (:shadowing-import-from "A" "B" "C"))
+      (parse-and-unparse
+       '(defpackage hello (:shadowing-import-from a b c)))))
 
 (define-test defpackage-export
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:export a b))))
+  (is #'equal
+      '(defpackage "HELLO" (:export "A" "B"))
+      (parse-and-unparse '(defpackage hello (:export a b)))))
 
 (define-test defpackage-intern
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:intern a b))))
+  (is #'equal
+      '(defpackage "HELLO" (:intern "A" "B"))
+      (parse-and-unparse '(defpackage hello (:intern a b)))))
 
 (define-test defpackage-size
   :parent defpackage
-  (compare-parse-and-unparse
-   '(defpackage hello (:size 234))))
+  (is #'equal
+      '(defpackage "HELLO" (:size 234))
+      (parse-and-unparse '(defpackage hello (:size 234)))))
