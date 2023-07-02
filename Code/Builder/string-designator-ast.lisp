93a84dfe8af70674972c5da5cbf9ed8d0601ddae
Robert Strandh <robert.strandh@gmail.com>
2023-07-02 07:31:16 +0200

    Methods on NODE-KIND and NODE-INITARGS for STRING-DESIGNATOR-AST.

--- Code/Builder/string-designator-ast.lisp
+++ Code/Builder/string-designator-ast.lisp
@@ -7,3 +7,10 @@
   (make-instance 'ico:string-designator-ast
     :origin source
     :designated-string string))
+
+(defmethod abp:node-kind ((builder builder) (node ico:string-designator-ast))
+  :string-designator)
+
+(defmethod abp:node-initargs
+    ((builder builder) (node ico:string-designator-ast))
+  `(:string ,(ico:designated-string node)))
