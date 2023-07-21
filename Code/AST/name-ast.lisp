a31ab49245574eae9d8eac32e8bbede789334149
Robert Strandh <robert.strandh@gmail.com>
2023-07-21 14:51:04 +0200

    Improve comments and introduce more subclasses.

--- Code/AST/name-ast.lisp
+++ Code/AST/name-ast.lisp
@@ -6,25 +6,62 @@
 (define-ast-class variable-name-ast (name-ast)
   ())
 
-;;; FIXME: We do not want to define a method on CHILDREN for this AST
-;;; because that would create a cycle in the AST graph.
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; VARIABLE-NAME-AST as an argument, once it is known that the
+;;; VARIABLE-NAME-AST corresponds to the definition of a variable.
 (define-ast-class variable-definition-ast (variable-name-ast)
-  (;; This slot contains a list of VARIABLE-REFERENCE-ASTs
+  (;; This slot contains a list of VARIABLE-REFERENCE-ASTs that
+   ;; correspond to references to the variable that is defined by this
+   ;; AST.
    (* variable-reference-asts)))
 
-;;; FIXME: We do not want to define a method on CHILDREN for this AST
-;;; because that would create a cycle in the AST graph.
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; VARIABLE-NAME-AST as an argument, once it is known that the
+;;; VARIABLE-NAME-AST corresponds to a variable reference.
 (define-ast-class variable-reference-ast (variable-name-ast)
-  (;; This slot contains the VARIABLE-DEFINITION-AST that defines the
+  (;; This slot contains the VARIABLE-DEFINITION-AST that corresponds
+   ;; to the definition of the variable that is referred to by this
+   ;; AST.
    (? variable-definition-ast)))
 
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; VARIABLE-NAME-AST as an argument, once it is known that the
+;;; VARIABLE-NAME-AST corresponds the binding of a special variable.
 (define-ast-class special-variable-bound-ast (variable-name-ast)
   ())
 
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; VARIABLE-NAME-AST as an argument, once it is known that the
+;;; VARIABLE-NAME-AST corresponds a reference to a special variable.
 (define-ast-class special-variable-reference-ast (variable-name-ast)
   ())
 
 (define-ast-class function-name-ast (name-ast)
+  ())
+
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; FUNCTION-NAME-AST as an argument, once it is known that the
+;;; FUNCTION-NAME-AST corresponds to the definition of a local
+;;; function name as in a LABELS or FLET form.
+(define-ast-class local-function-name-definition-ast (function-name-ast)
+  (;; This slot contains a list of FUNCTION-REFERENCE-ASTs that
+   ;; correspond to references to the function name that is defined by
+   ;; this AST.
+   (* local-function-name-reference-asts)))
+
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; FUNCTION~-NAME-AST as an argument, once it is known that the
+;;; FUNCTION-NAME-AST corresponds to a reference to a local function.
+(define-ast-class function-reference-ast (function-name-ast)
+  (;; This slot contains the FUNCTION-DEFINITION-AST that corresponds
+   ;; to the definition of the local function that is referred to by
+   ;; this AST.
+   (? local-function-name-definition-ast)))
+
+;;; This class can be used in a call to CHANGE-CLASS with a
+;;; FUNCTION-NAME-AST as an argument, once it is known that the
+;;; FUNCTION-NAME-AST corresponds a reference to a global-function.
+(define-ast-class global-function-name-reference-ast (function-name-ast)
   ())
 
 (define-ast-class type-name-ast (name-ast)
