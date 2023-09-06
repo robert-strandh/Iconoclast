7d1f5f03c172b53e4b91737ca6e978286b1cc1c7
Robert Strandh <robert.strandh@gmail.com>
2023-09-06 07:36:20 +0200

    Export SPECIAL-FORM-AST, MACRO-FORM-AST, FUNCTION-FORM-AST.

--- Code/AST/packages.lisp
+++ Code/AST/packages.lisp
@@ -5,6 +5,9 @@
   (:shadow #:documentation)
   (:export
      #:ast
+     #:special-form-ast
+     #:macro-form-ast
+     #:function-form-ast
      #:origin
      #:children
      #:slot-designators
