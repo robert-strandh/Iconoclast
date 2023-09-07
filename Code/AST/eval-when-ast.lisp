c38e2ad89aea81417ccc16e40ed02b691e1a837f
Robert Strandh <robert.strandh@gmail.com>
2023-09-07 15:03:07 +0200

    Make EVAL-WHEN-AST a subclass of SPECIAL-FORM-AST.

--- Code/AST/eval-when-ast.lisp
+++ Code/AST/eval-when-ast.lisp
@@ -3,5 +3,5 @@
 (define-ast-class situation-ast (name-ast)
   ())
 
-(define-ast-class eval-when-ast (form-asts-mixin macro-form-ast)
+(define-ast-class eval-when-ast (form-asts-mixin special-form-ast)
   ((* situation-asts)))
