630a7ca9b5a2ce416dc9c7f274d10c938c5460a4
Robert Strandh <robert.strandh@gmail.com>
2023-09-19 10:34:22 +0200

    Use new function DISPLAY-SIMPLE-DECLARATION-AST to facto code.

--- Code/Visualizer/ignore-ast.lisp
+++ Code/Visualizer/ignore-ast.lisp
@@ -1,23 +1,7 @@
 (cl:in-package #:iconoclast-visualizer)
 
 (defmethod display-ast* ((ast ico:ignore-ast) pane hpos vpos)
-  (let* ((name "ignore")
-         (width (+ (clim:stream-string-width pane name) 10))
-         (height 20))
-    (draw-ast pane hpos vpos width height name)
-    (let ((child-vpos vpos))
-      (setf child-vpos
-            (display-asts (ico:name-asts ast)
-                          pane (+ hpos width 10) child-vpos))
-      (+ child-vpos height 10))))
+  (display-simple-declaration-ast ast "ignore" pane hpos vpos))
 
 (defmethod display-ast* ((ast ico:ignorable-ast) pane hpos vpos)
-  (let* ((name "ignorable")
-         (width (+ (clim:stream-string-width pane name) 10))
-         (height 20))
-    (draw-ast pane hpos vpos width height name)
-    (let ((child-vpos vpos))
-      (setf child-vpos
-            (display-asts (ico:name-asts ast)
-                          pane (+ hpos width 10) child-vpos))
-      (+ child-vpos height 10))))
+  (display-simple-declaration-ast ast "ignorable" pane hpos vpos))
--- Code/Visualizer/inline-ast.lisp
+++ Code/Visualizer/inline-ast.lisp
@@ -1,23 +1,7 @@
 (cl:in-package #:iconoclast-visualizer)
 
 (defmethod display-ast* ((ast ico:inline-ast) pane hpos vpos)
-  (let* ((name "inline")
-         (width (+ (clim:stream-string-width pane name) 10))
-         (height 20))
-    (draw-ast pane hpos vpos width height name)
-    (let ((child-vpos vpos))
-      (setf child-vpos
-            (display-asts (ico:name-asts ast)
-                          pane (+ hpos width 10) child-vpos))
-      (+ child-vpos height 10))))
+  (display-simple-declaration-ast ast "inline" pane hpos vpos))
 
 (defmethod display-ast* ((ast ico:notinline-ast) pane hpos vpos)
-  (let* ((name "notinline")
-         (width (+ (clim:stream-string-width pane name) 10))
-         (height 20))
-    (draw-ast pane hpos vpos width height name)
-    (let ((child-vpos vpos))
-      (setf child-vpos
-            (display-asts (ico:name-asts ast)
-                          pane (+ hpos width 10) child-vpos))
-      (+ child-vpos height 10))))
+  (display-simple-declaration-ast ast "notinline" pane hpos vpos))
