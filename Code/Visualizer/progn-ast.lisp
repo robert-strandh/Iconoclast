71bd7c408cf3c3ebc5a3f68bef8b032a1962aaf9
Robert Strandh <robert.strandh@gmail.com>
2023-09-19 10:08:19 +0200

    Use new utlity DISPLAY-ASTS for displaying PROGN-AST.

--- Code/Visualizer/progn-ast.lisp
+++ Code/Visualizer/progn-ast.lisp
@@ -6,10 +6,7 @@
          (height 20))
     (draw-ast pane hpos vpos width height name)
     (let ((child-vpos vpos))
-      (loop for child in (ico:form-asts ast)
-            do (setf child-vpos
-                     (+ (display-ast* child pane
-                                      (+ hpos width 10)
-                                      child-vpos)
-                        10)))
-      (+ child-vpos height))))
+      (incf child-vpos
+            (display-asts (ico:form-asts ast)
+                          pane (+ hpos width 10) child-vpos))
+      (+ child-vpos height 10))))
