7bcb12b16dc5cd9a8a15c435c61b9163e0c0925c
Robert Strandh <robert.strandh@gmail.com>
2023-09-19 10:34:06 +0200

    Define function DISPLAY-SIMPLE-DECLARATION-AST.

--- Code/Visualizer/utilities.lisp
+++ Code/Visualizer/utilities.lisp
@@ -6,3 +6,13 @@
     (loop for ast in asts
           do (incf child-vpos (display-ast* ast pane hpos child-vpos)))
     child-vpos))
+
+(defun display-simple-declaration-ast (ast name pane hpos vpos)
+  (let* ((width (+ (clim:stream-string-width pane name) 10))
+         (height 20))
+    (draw-ast pane hpos vpos width height name)
+    (let ((child-vpos vpos))
+      (setf child-vpos
+            (display-asts (ico:name-asts ast)
+                          pane (+ hpos width 10) child-vpos))
+      (+ child-vpos height 10))))
