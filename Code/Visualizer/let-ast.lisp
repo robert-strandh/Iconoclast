5570f4e2cf79f281556c895447581ddfef5513cb
Robert Strandh <robert.strandh@gmail.com>
2023-09-19 10:06:16 +0200

    Use new utlity DISPLAY-ASTS.

--- Code/Visualizer/let-ast.lisp
+++ Code/Visualizer/let-ast.lisp
@@ -5,25 +5,16 @@
          (height 20))
     (draw-ast pane hpos vpos width height name)
     (let ((child-vpos vpos))
-      (loop for variable-binding-ast in (ico:binding-asts ast)
-            do (setf child-vpos
-                     (+ (display-ast* variable-binding-ast pane
-                                      (+ hpos width 20)
-                                      child-vpos)
-                        10)))
-      (loop for declaration-ast in (ico:declaration-asts ast)
-            do (setf child-vpos
-                     (+ (display-ast* declaration-ast pane
-                                      (+ hpos width 15)
-                                      child-vpos)
-                        10)))
-      (loop for form-ast in (ico:form-asts ast)
-            do (setf child-vpos
-                     (+ (display-ast* form-ast pane
-                                      (+ hpos width 10)
-                                      child-vpos)
-                        10)))
-      (+ child-vpos height))))
+      (incf child-vpos
+            (display-asts (ico:binding-asts ast)
+                          pane (+ hpos width 20) child-vpos))
+      (incf child-vpos
+            (display-asts (ico:declaration-asts ast)
+                          pane (+ hpos width 15) child-vpos))
+      (incf child-vpos
+            (display-asts (ico:form-asts ast)
+                          pane (+ hpos width 10) child-vpos))
+      (+ child-vpos height 10))))
 
 (defmethod display-ast* ((ast ico:let-ast) pane hpos vpos)
   (display-let-or-let* "let" ast pane hpos vpos))
