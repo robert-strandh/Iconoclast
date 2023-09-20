71ec858a9ad78351ffc35c578105fee00943d9de
Robert Strandh <robert.strandh@gmail.com>
2023-09-20 15:53:54 +0200

    Define more methods on LAYOUT.

--- Code/Visualizer/layout.lisp
+++ Code/Visualizer/layout.lisp
@@ -137,4 +137,180 @@
     (40 ico:shadowing-import-from-asts)
     (40 ico:size-ast)))
 
-    
+(defmethod layout ((ast ico:defvar-ast))
+  `((20 ico:variable-name-ast)
+    (20 ico:form-ast)))
+
+(defmethod layout ((ast ico:defparameter-ast))
+  `((20 ico:variable-name-ast)
+    (20 ico:form-ast)))
+
+(defmethod layout ((ast ico:defun-ast))
+  `((20 ico:name-ast)
+    (60 ico:lambda-list-ast)
+    (40 ico:declaration-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:defclass-ast))
+  `((20 ico:name-ast)
+    (60 ico:superclass-asts)
+    (40 ico:metaclass-ast)
+    (30 ico:slot-specifier-asts)
+    (20 ico:default-initarg-asts)))
+
+(defmethod layout ((ast ico:define-condition-ast))
+  `((20 ico:name-ast)
+    (60 ico:parent-type-asts)
+    (40 ico:metaclass-ast)
+    (30 ico:slot-specifier-asts)
+    (20 ico:default-initarg-asts)
+    (20 ico:report-ast)))
+
+(defmethod layout ((ast ico:defgeneric-ast))
+  `((20 ico:name-ast)
+    (40 ico:lambda-list-ast)
+    (20 ico:argument-precedence-order-asts)
+    (20 ico:generic-function-class-ast)
+    (20 ico:method-class-ast)
+    (20 ico:method-combination-name-and-arguments-ast)
+    (20 ico:method-description-asts)))
+
+(defmethod layout ((ast ico:declaim-ast))
+  `((20 ico:declaration-specifier-asts)))
+
+(defmethod layout ((ast ico:lambda-ast))
+  `((20 ico:lambda-list-ast)
+    (40 ico:declaration-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:prog-or-prog*-ast))
+  `((40 ico:binding-asts)
+    (20 ico:segment-asts)))
+
+(defmethod layout ((ast ico:prog1-ast))
+  `((40 ico:first-form-ast)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:prog2-ast))
+  `((60 ico:first-form-ast)
+    (40 ico:second-form-ast)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:pushnew-ast))
+  `((20 ico:item-ast)
+    (20 ico:place-ast)
+    (20 ico:key-ast)
+    (20 ico:test-ast)
+    (20 ico:test-not-ast)))
+
+(defmethod layout ((ast ico:remf-ast))
+  `((20 ico:indicator-ast)
+    (20 ico:place-ast)))
+
+(defmethod layout ((ast ico:ignore-errors-ast))
+  `((20 ico:form-asts)))
+
+(defmethod layout ((ast ico:in-package-ast))
+  `((20 ico:name-ast)))
+
+(defmethod layout ((ast ico:check-type-ast))
+  `((20 ico:place-ast)
+    (20 ico:typespec-ast)))
+
+(defmethod layout ((ast ico:dolist-ast))
+  `((40 ico:variable-name-ast)
+    (40 ico:list-form-ast)
+    (40 ico:result-ast)
+    (20 ico:segment-asts)))
+
+(defmethod layout ((ast ico:dotimes-ast))
+  `((40 ico:variable-name-ast)
+    (40 ico:count-form-ast)
+    (40 ico:result-ast)
+    (20 ico:segment-asts)))
+  
+(defmethod layout ((ast ico:do-iteration-variable-ast))
+  `((20 ico:do-variable-name-ast)
+    (20 ico:init-form-ast)
+    (20 ico:step-form-ast)))
+
+(defmethod layout ((ast ico:do-do*-ast))
+  `((40 ico:do-iteration-variable-asts)
+    (40 ico:end-test-ast)
+    (40 ico:result-asts)
+    (20 ico:segment-asts)))
+
+(defmethod layout ((ast ico:setf-or-psetf-ast))
+  `((20 ico:place-asts)
+    (20 ico:value-asts)))
+
+(defmethod layout ((ast ico:print-unreadable-object-ast))
+  `((20 ico:object-ast)
+    (20 ico:stream-ast)
+    (20 ico:type-ast)
+    (20 ico:identity-ast)))
+
+(defmethod layout ((ast ico:setq-or-psetq-ast))
+  `((20 ico:variable-name-asts)
+    (20 ico:value-asts)))
+
+(defmethod layout ((ast ico:typecase-or-etypecase-ast))
+  `((20 ico:keyform-ast)
+    (20 ico:clause-asts)))
+
+(defmethod layout ((ast ico:ctypecase-ast))
+  `((20 ico:keyplace-ast)
+    (20 ico:clause-asts)))
+  
+(defmethod layout ((ast ico:with-accessors-slot-entry-ast))
+  `((20 ico:variable-name-ast)
+    (20 ico:accessor-name-ast)))
+
+(defmethod layout ((ast ico:with-accessors-ast))
+  `((60 ico:slot-entry-asts)
+    (40 ico:instance-form-ast)
+    (30 ico:declaration-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:with-input-from-string-ast))
+  `((20 ico:var-ast)
+    (20 ico:string-ast)
+    (20 ico:index-ast)
+    (20 ico:start-ast)
+    (20 ico:end-ast)))
+
+(defmethod layout ((ast ico:with-open-file-ast))
+  `((40 ico:stream-ast)
+    (40 ico:filespec-ast)
+    (30 ico:option-asts)
+    (30 ico:declaration-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:with-open-stream-ast))
+  `((40 ico:var-ast)
+    (40 ico:stream-ast)
+    (30 ico:declaration-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:with-output-to-string-ast))
+  `((40 ico:var-ast)
+    (40 ico:string-ast)
+    (40 ico:element-type-ast)
+    (30 ico:declaration-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:with-simple-restart-ast))
+  `((40 ico:name-ast)
+    (40 ico:format-control-ast)
+    (40 ico:format-argument-asts)
+    (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:slot-entry-ast))
+  `((20 ico:variable-name-ast)
+    (20 ico:slot-name-ast)))
+
+(defmethod layout ((ast ico:with-slots-ast))
+  `((60 ico:slot-entry-asts)
+    (40 ico:instance-form-ast)
+    (30 ico:declaration-asts)
+    (20 ico:form-asts)))
