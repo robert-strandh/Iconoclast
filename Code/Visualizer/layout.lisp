12a12cbb1bc43a39639e03d5e56dc7b11f55154b
Robert Strandh <robert.strandh@gmail.com>
2023-09-20 07:01:33 +0200

    Define layouts for APPLICATION-AST and LAMBDA-EXPRESSION-AST.

--- Code/Visualizer/layout.lisp
+++ Code/Visualizer/layout.lisp
@@ -11,3 +11,11 @@
     (20 ico:declaration-asts)
     (10 ico:form-asts)))
 
+(defmethod layout ((ast ico:application-ast))
+  '((20 ico:function-name-ast)
+    (30 ico:argument-asts)))
+
+(defmethod layout ((ast ico:lambda-expression-ast))
+  '((40 ico:lambda-list-ast)
+    (30 ico:declaration-asts)
+    (20 ico:form-asts)))
