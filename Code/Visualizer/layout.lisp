62cb4f51f40117fd819eb511aa0889eb7de561f5
Robert Strandh <robert.strandh@gmail.com>
2023-09-20 07:22:29 +0200

    Define methods on LAYOUT for ORDINARY-LAMBDA-LIST.

--- Code/Visualizer/layout.lisp
+++ Code/Visualizer/layout.lisp
@@ -19,3 +19,16 @@
   '((40 ico:lambda-list-ast)
     (30 ico:declaration-asts)
     (20 ico:form-asts)))
+
+(defmethod layout ((ast ico:single-parameter-section-ast))
+  `((20 ico:parameter-ast)))
+
+(defmethod layout ((ast ico:multi-parameter-section-ast))
+  `((20 ico:parameter-asts)))
+
+(defmethod layout ((ast ico:ordinary-lambda-list-ast))
+  `((20 ico:required-section-ast)
+    (20 ico:optional-section-ast)
+    (20 ico:rest-section-ast)
+    (20 ico:key-section-ast)
+    (20 ico:aux-section-ast)))
