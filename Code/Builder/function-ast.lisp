f26c587491170d4285d9c2b3c1e451c9b6e1a716
Robert Strandh <robert.strandh@gmail.com>
2023-11-07 10:05:29 +0100

    Define method on NODE-RELATIONS for FUNCTION-AST.

--- Code/Builder/function-ast.lisp
+++ Code/Builder/function-ast.lisp
@@ -8,3 +8,8 @@
 (define-relations ico:function-ast
   ((:name ico:function-name-ast ico:name-ast)
    (:lambda t ico:name-ast)))
+
+(defmethod abp:node-relations (builder (ast ico:function-ast))
+  (if (typep (ico:name-ast ast) 'ico:lambda-expression-ast)
+      '((:lambda . 1))
+      '((:name . 1))))
