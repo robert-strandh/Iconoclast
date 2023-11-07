22acfc4fed529b7043a40f00247b0284c9f4170a
Robert Strandh <robert.strandh@gmail.com>
2023-11-07 11:38:56 +0100

    Add more test cases.

--- Code/AST-transformations/Test/let-to-labels.lisp
+++ Code/AST-transformations/Test/let-to-labels.lisp
@@ -12,3 +12,24 @@
       '(labels ((#1=#:f () 234)) (#1#))
       (parse-let-to-labels-and-unparse
        '(let () 234))))
+
+(define-test let-to-labels-one-binding-no-initform
+  :parent let-to-labels
+  (is #'forms-similar-p
+      '(labels ((#1=#:f (x) x)) (#1# nil))
+      (parse-let-to-labels-and-unparse
+       '(let (x) x))))
+
+(define-test let-to-labels-one-binding-with-initform
+  :parent let-to-labels
+  (is #'forms-similar-p
+      '(labels ((#1=#:f (x) x)) (#1# 234))
+      (parse-let-to-labels-and-unparse
+       '(let ((x 234)) x))))
+
+(define-test let-to-labels-two-bindings
+  :parent let-to-labels
+  (is #'forms-similar-p
+      '(labels ((#1=#:f (x y) x y)) (#1# 234 345))
+      (parse-let-to-labels-and-unparse
+       '(let ((x 234) (y 345)) x y))))
