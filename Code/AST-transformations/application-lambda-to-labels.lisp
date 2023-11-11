bc8735a5c7b77c0b15808d3e5133dd1e24c18c79
Robert Strandh <robert.strandh@gmail.com>
2023-11-11 09:54:50 +0100

    Separate transformation code from traversal code.

--- Code/AST-transformations/application-lambda-to-labels.lisp
+++ Code/AST-transformations/application-lambda-to-labels.lisp
@@ -10,6 +10,33 @@
 ;;;;
 ;;;; where <name> is a generated symbol.
 
+(defun application-lambda-to-labels (application-ast)
+  (let* ((function-name-definition-ast
+           (make-instance 'ico:local-function-name-definition-ast
+             :name (gensym)))
+         (function-name-reference-ast
+           (make-instance 'ico:function-reference-ast
+             :name (ico:name function-name-definition-ast)
+             :local-function-name-definition-ast
+             function-name-definition-ast))
+         (operator-ast (ico:function-name-ast application-ast)))
+    (reinitialize-instance function-name-definition-ast
+      :local-function-name-reference-asts
+      (list function-name-reference-ast))
+    (make-instance 'ico:labels-ast
+      :binding-asts
+      (list (make-instance 'ico:local-function-ast
+              :name-ast function-name-definition-ast
+              :lambda-list-ast (ico:lambda-list-ast operator-ast)
+              :declaration-asts (ico:declaration-asts operator-ast)
+              :form-asts (ico:form-asts operator-ast)
+              :origin (ico:origin operator-ast)))
+      :form-asts
+      (list (make-instance 'ico:application-ast
+              :function-name-ast function-name-reference-ast
+              :argument-asts (ico:argument-asts application-ast)
+              :origin (ico:origin application-ast))))))
+
 (defclass application-lambda-to-labels-client (client) ())
 
 (defmethod iaw:walk-ast-node :around
@@ -18,32 +45,9 @@
   (call-next-method)
   (let ((operator-ast (ico:function-name-ast ast)))
     (if (typep operator-ast 'ico:lambda-expression-ast)
-        (let* ((function-name-definition-ast
-                 (make-instance 'ico:local-function-name-definition-ast
-                   :name (gensym)))
-               (function-name-reference-ast
-                 (make-instance 'ico:function-reference-ast
-                   :name (ico:name function-name-definition-ast)
-                   :local-function-name-definition-ast
-                   function-name-definition-ast)))
-          (reinitialize-instance function-name-definition-ast
-            :local-function-name-reference-asts
-            (list function-name-reference-ast))
-          (make-instance 'ico:labels-ast
-            :binding-asts
-            (list (make-instance 'ico:local-function-ast
-                    :name-ast function-name-definition-ast
-                    :lambda-list-ast (ico:lambda-list-ast operator-ast)
-                    :declaration-asts (ico:declaration-asts operator-ast)
-                    :form-asts (ico:form-asts operator-ast)
-                    :origin (ico:origin operator-ast)))
-            :form-asts
-            (list (make-instance 'ico:application-ast
-                    :function-name-ast function-name-reference-ast
-                    :argument-asts (ico:argument-asts ast)
-                    :origin (ico:origin ast)))))
+        (application-lambda-to-labels ast)
         ast)))
                   
-(defun application-lambda-to-labels (ast)
+(defun application-lambda-to-labels-everywhere (ast)
   (let ((client (make-instance 'application-lambda-to-labels-client)))
     (iaw:walk-ast client ast)))
