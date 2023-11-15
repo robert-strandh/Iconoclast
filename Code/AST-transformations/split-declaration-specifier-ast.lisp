df8bad307733c727f1eaec3b49e787da54169baf
Robert Strandh <robert.strandh@gmail.com>
2023-11-15 16:11:36 +0100

    Change contract of function SPLIT-DECLARATION-SPECIFIER-AST.

--- Code/AST-transformations/split-declaration-specifier-ast.lisp
+++ Code/AST-transformations/split-declaration-specifier-ast.lisp
@@ -1,26 +1,25 @@
 (cl:in-package #:iconoclast-ast-transformations)
 
-;;; This function takes a DECLARATION-SPECIFIER-AST and returns two
-;;; lists as two return values.  Each element of first list is of the
-;;; form (<variable-name-ast> . <declaration-specifier-ast>) such that
-;;; <declaration-specifier-ast> is a DECLARATION-SPECIFIER-AST of the
-;;; same type as the argument to this function, and it contains a
-;;; single VARIABLE-NAME-AST that can be either a
-;;; VARIABLE-REFERENCE-AST or a SPECIAL-VARIABLE-REFERENCE-AST
-;;; according to whether the variable ls lexical or special.  The
-;;; <variable-name-ast> is the same as the one contained in
-;;; <declaration-specifier-ast>.  Each element of the second list is a
+;;; This function takes a RESTRICTING-DECLARATION-SPECIFIER-AST and
+;;; returns a list of pairs (<name-ast> . <declaration-specifier-ast>)
+;;; such that <declaration-specifier-ast> is a
 ;;; DECLARATION-SPECIFIER-AST of the same type as the argument to this
-;;; function, such that either the DECLARATION-SPECIFIER-AST does not
-;;; refer to any names at all, or it refers to function names rather
-;;; than variable names.
+;;; function, and it contains a single NAME-AST that can be either a
+;;; VARIABLE-REFERENCE-AST, a SPECIAL-VARIABLE-REFERENCE-AST, a
+;;; FUNCTION-REFERENCE-AST, or a GLOBAL-FUNCTION-NAME-REFERENCE-AST.
+;;; The <name-ast> is the same as the one contained in
+;;; <declaration-specifier-ast>.
+;;;
+;;; We do it this way because client code is allowed to define
+;;; specific DECLARATION-SPECIFIER-AST types, and we can't know how to
+;;; create one of those, so for such a specific AST type, we require
+;;; client code to define a method on this function.
 
-(defgeneric split-declaration-specifier-ast (declaration-specifier-ast))
+(defgeneric split-declaration-specifier-ast
+    (restricted-declaration-specifier-ast))
 
-(defmethod split-declaration-specifier-ast (ast)
-  (values '() (list ast)))
-                                            
-(defun split-simple-declaration-specifier-ast (ast)
+(defmethod split-declaration-specifier-ast
+    ((ast ico:restricted-declaration-specifier-ast))
   (loop with origin = (ico:origin ast)
         with class = (class-of ast)
         for name-ast in (ico:name-asts ast)
@@ -31,37 +30,20 @@
                 :name (ico:name (ico:identifier-ast ast)))
               :name-asts (list name-ast)
               :origin origin)
-        if (typep name-ast 'ico:function-name-ast)
-          collect declaration-specifier-ast into result2
-        else
-          collect (cons name-ast declaration-specifier-ast) into result1
-        finally (return (values result1 result2))))
+        collect (cons name-ast declaration-specifier-ast)))
 
-(defmethod split-declaration-specifier-ast ((ast ico:dynamic-extent-ast))
-  (split-simple-declaration-specifier-ast ast))
-
-(defmethod split-declaration-specifier-ast ((ast ico:ignore-ast))
-  (split-simple-declaration-specifier-ast ast))
-
-(defmethod split-declaration-specifier-ast ((ast ico:ignorable-ast))
-  (split-simple-declaration-specifier-ast ast))
-
-(defmethod split-declaration-specifier-ast ((ast ico:type-ast))
-  (values (loop with type-specifier-ast = (ico:type-specifier-ast ast)
-                with origin = (ico:origin ast)
-                for name-ast in (ico:name-asts ast)
-                collect (cons name-ast
-                              (make-instance 'ico:type-ast
-                                :type-specifier-ast type-specifier-ast
-                                :name-ast name-ast
-                                :origin origin)))
-          '()))
-
-(defmethod split-declaration-specifier-ast ((ast ico:special-ast))
-  (values (loop with origin = (ico:origin ast)
-                for name-ast in (ico:name-asts ast)
-                collect (cons name-ast
-                              (make-instance 'ico:special-ast
-                                :name-ast name-ast
-                                :origin origin)))
-          '()))
+(defmethod split-declaration-specifier-ast
+    ((ast ico:type-or-ftype-ast))
+  (loop with origin = (ico:origin ast)
+        with class = (class-of ast)
+        with type-specifier-ast = (ico:type-specifier-ast ast)
+        for name-ast in (ico:name-asts ast)
+        for declaration-specifier-ast
+          = (make-instance class
+              :identifier-ast
+              (make-instance 'ico:name-ast
+                :name (ico:name (ico:identifier-ast ast)))
+              :type-specifier-ast type-specifier-ast
+              :name-asts (list name-ast)
+              :origin origin)
+        collect (cons name-ast declaration-specifier-ast)))
