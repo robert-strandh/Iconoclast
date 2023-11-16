0f4c8162e982cc7a77feaede9c78b87a6867ac58
Robert Strandh <robert.strandh@gmail.com>
2023-11-16 12:38:17 +0100

    Rewrite SPLIT-LET-OR-LET* using a dictionary for declarations.

--- Code/AST-transformations/split-let-or-letstar-ast.lisp
+++ Code/AST-transformations/split-let-or-letstar-ast.lisp
@@ -1,72 +1,61 @@
 (cl:in-package #:iconoclast-ast-transformations)
 
-(defclass split-let-or-letstar-client (client) ())
+(defclass split-let-or-let*-client (client) ())
 
-(defun compute-declaration-ast-associations (ast)
-  (let ((declaration-specifier-asts
-          (extract-declaration-specifier-asts (ico:declaration-asts ast))))
-    (let ((variable-ast-pairs '())
-          (other-asts '()))
-      (loop for declaration-specifier-ast in declaration-specifier-asts
-            do (if (typep declaration-specifier-ast
-                          'ico:restricting-declaration-specifier-ast)
-                   (let ((pairs (split-declaration-specifier-ast
-                                 declaration-specifier-ast)))
-                     (loop for pair in pairs
-                           for (name-ast . declaration-specifier-ast)
-                             = pair
-                           do (if (typep name-ast 'ico:variable-name-ast)
-                                  (push pair variable-ast-pairs)
-                                  (push declaration-specifier-ast
-                                        other-asts))))
-                   (push declaration-specifier-ast other-asts)))
-      (let ((variable-name-asts
-              (loop for variable-binding-ast in (ico:binding-asts ast)
-                    collect (ico:variable-name-ast variable-binding-ast))))
-        (multiple-value-bind (associated-asts remaining-asts)
-            (associate-variable-asts-and-declaration-specifier-asts
-             variable-name-asts variable-ast-pairs)
-          (values associated-asts (append remaining-asts other-asts)))))))
+;;; This function assumes that the AST has at least one BINDING-AST.
+;;; It splits the AST into two nested LET-ASTs where the outer one
+;;; contains the first BINDING-AST and the inner one contains the
+;;; remaining BINDING-ASTs.
+(defun split-let-or-let*-once (ast dictionary)
+  (let* ((binding-asts (ico:binding-asts ast))
+         (binding-ast (first binding-asts))
+         (variable-ast (ico:variable-name-ast binding-ast))
+         (key (if (typep variable-ast 'ico:special-variable-bound-ast)
+                  (ico:name variable-ast)
+                  variable-ast))
+         (declaration-specifier-asts
+           (declaration-specifier-asts dictionary key))
+         (inner (make-instance 'ico:let-ast
+                  :form-asts (ico:form-asts ast)
+                  :binding-asts (rest binding-asts))))
+    (remove-declaration-specifier-asts dictionary key)
+    (reinitialize-instance ast
+      :binding-asts (list binding-ast)
+      :declartion-asts
+      (list (make-instance 'ico:declare-ast
+              :declaration-specifier-asts declaration-specifier-asts
+              :form-asts (list inner))))))
 
-(defun split-let-or-let-star-ast-helper (ast)
-  (multiple-value-bind (associated-asts other-asts)
-      (compute-declaration-ast-associations ast)
-    (labels ((split (ast binding-asts associated-asts)
-               (if (null binding-asts)
-                   (change-class
-                    ast 'ico:locally-ast
-                    :declaration-asts
-                    (list (make-instance 'ico:declaration-ast
-                            :declaration-specifier-asts other-asts)))
-                   (let ((inner
-                           (make-instance 'ico:let-ast
-                             :form-asts (ico:form-asts ast))))
-                     (reinitialize-instance ast
-                       :binding-asts (list (first binding-asts))
-                       :declaration-asts
-                       (list (make-instance 'ico:declaration-ast
-                               :declaration-specifier-asts 
-                               (first associated-asts)))
-                       :form-asts (list inner))
-                     (split inner
-                            (rest binding-asts)
-                            (rest associated-asts))))
-               ast))
-      (split ast (ico:binding-asts ast) associated-asts))))
+(defun split-let-or-let*-ast-helper (ast)
+  (let* ((declaration-asts (ico:declaration-asts ast))
+         (dictionary (create-declaration-dictionary declaration-asts))
+         (ast-to-split ast))
+    (loop until (null (ico:binding-asts ast-to-split))
+          do (split-let-or-let*-once ast-to-split dictionary)
+             (setq ast-to-split (first (ico:form-asts ast-to-split))))
+    (let ((declaration-specifier-asts
+            (loop for value being each hash-value
+                    of dictionary
+                  append value)))
+      (change-class
+       ast 'ico:locally-ast
+       :declaration-asts
+       (list (make-instance 'ico:declare-ast
+               :declaraton-specifier-asts declaration-specifier-asts))))))
 
 (defmethod iaw:walk-ast-node :around
-    ((client split-let-or-letstar-client) (ast ico:let-ast))
+    ((client split-let-or-let*-client) (ast ico:let-ast))
   ;; Start by converting any children of this AST node.
   (call-next-method)
-  (split-let-or-let-star-ast-helper ast))
+  (split-let-or-let*-ast-helper ast))
 
 (defmethod iaw:walk-ast-node :around
-    ((client split-let-or-letstar-client) (ast ico:let*-ast))
+    ((client split-let-or-let*-client) (ast ico:let*-ast))
   ;; Start by converting any children of this AST node.
   (call-next-method)
   (change-class ast 'ico:let-ast)
-  (split-let-or-let-star-ast-helper ast))
+  (split-let-or-let*-ast-helper ast))
 
-(defun split-let-or-letstar-ast (ast)
-  (let ((client (make-instance 'split-let-or-letstar-client)))
+(defun split-let-or-let*-ast (ast)
+  (let ((client (make-instance 'split-let-or-let*-client)))
     (iaw:walk-ast client ast)))
