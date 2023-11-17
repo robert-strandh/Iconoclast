14ad201be59f463907fbd0d32e9d5916d443f4f1
Robert Strandh <robert.strandh@gmail.com>
2023-11-17 15:39:46 +0100

    Define function EXTRACT-SPECIAL-DECLARED-VARIABLE-ASTS.

--- Code/AST-transformations/extract-declaration-specifier-asts.lisp
+++ Code/AST-transformations/extract-declaration-specifier-asts.lisp
@@ -5,3 +5,14 @@
 (defun extract-declaration-specifier-asts (declaration-asts)
   (loop for declaration-ast in declaration-asts
         append (ico:declaration-specifier-asts declaration-ast)))
+
+;;; This function takes a list of DECLARATION-ASTs, and return a list
+;;; of VARIABLE-NAME-ASTs (with possible duplicates) that occur in any
+;;; SPECIAL-AST in any of the DECLARATION-SPECIFIER-ASTs in any of the
+;;; DECLARATION-ASTs passed as an argument.
+(defun extract-special-declared-variable-asts (declaration-asts)
+  (let ((declaraton-specifier-asts
+          (extract-declaration-specifier-asts declaration-asts)))
+    (loop for declaraton-specifier-ast in declaraton-specifier-asts
+          when (typep declaraton-specifier-ast 'ico:special-ast)
+            append (ico:name-asts declaraton-specifier-ast))))
