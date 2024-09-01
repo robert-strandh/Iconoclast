a997fbf5a165a06dd5ed5de0581b807f39d76759
Robert Strandh <robert.strandh@gmail.com>
2024-09-01 14:01:10 +0200

    Add comments explaining the use of the AST classes.

--- Code/AST/write-cell-ast.lisp
+++ Code/AST/write-cell-ast.lisp
@@ -1,5 +1,9 @@
 (cl:in-package #:iconoclast)
 
+;;; This AST class is introduced by closure conversion.  A SETQ-AST
+;;; with a reference to a shared lexical variable is replaced by an
+;;; instance of this AST class.
+
 (define-ast-class write-cell-ast (form-ast-mixin ast)
   (;; This slot contains a FORM-AST that evaluates to a cell,
    ;; typically either a VARIABLE-REFERENCE-AST or a
