a09a91d76b155e3bcb15d0704ec09a27c1787307
Robert Strandh <robert.strandh@gmail.com>
2024-09-03 13:28:33 +0200

    Define main entry point and method on WALK-AST-NODE.

--- Code/Closure-conversion/function-tree.lisp
+++ Code/Closure-conversion/function-tree.lisp
@@ -5,5 +5,26 @@
 (defgeneric children (tree-node))
 
 (defclass tree-node ()
-  ((%function-ast :initarg :function-ast :reader function-ast)
-   (%children :initform '() :accessor children)))
+  ((%function-ast
+    :initform '()
+    :initarg :function-ast
+    :reader function-ast)
+   (%children
+    :initform '()
+    :accessor children)))
+
+(defclass tree-node-client () ())
+
+(defvar *current-node*)
+
+(defmethod aw:walk-ast-node :around
+    ((client tree-node-client) (ast ico:local-function-ast))
+  (let ((new-node (make-instance 'tree-node :function-ast ast)))
+    (push new-node (children *current-node*))
+    (let ((*current-node* new-node))
+      (call-next-method))))
+
+(defun compute-function-tree (ast)
+  (let ((*current-node* (make-instance 'tree-node))
+        (client (make-instance 'tree-node-client)))
+    (aw:walk-ast client ast)))
