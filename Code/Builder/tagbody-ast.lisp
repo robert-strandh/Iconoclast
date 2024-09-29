17bfb8ad1e78e4065d8254e482a19377df40df80
Robert Strandh <robert.strandh@gmail.com>
2024-09-29 09:00:07 +0200

    Add :AFTER methods on RELATE to link to parents.

--- Code/Builder/tagbody-ast.lisp
+++ Code/Builder/tagbody-ast.lisp
@@ -9,3 +9,19 @@
 (define-relations ico:tagbody-segment-ast
   ((:label ico:tag-ast ico:tag-ast)
    (:statement t ico:statement-asts)))
+
+(defmethod abp:relate :after
+    ((builder builder)
+     (relation (eql :label))
+     (left ico:tagbody-segment-ast)
+     (right ico:tag-ast)
+     &key)
+  (reinitialize-instance right :segment-ast left))
+
+(defmethod abp:relate :after
+    ((builder builder)
+     (relation (eql :segment))
+     (left ico:tagbody-ast)
+     (right ico:tagbody-segment-ast)
+     &key)
+  (reinitialize-instance right :tagbody-ast left))
