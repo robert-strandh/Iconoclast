b65ed9fd7fe04b8eb5bc7a0edc88654fa60d6a57
Robert Strandh <robert.strandh@gmail.com>
2024-10-02 05:41:54 +0200

    Add comment.

--- Code/AST/go-with-variable-ast.lisp
+++ Code/AST/go-with-variable-ast.lisp
@@ -1,5 +1,14 @@
 (cl:in-package #:iconoclast)
 
+;;; This AST class is used in conjunction with the
+;;; TAGBODY-WITH-VARIABLE-AST when the shared variable between TAGBODY
+;;; and GO must be made explicit, typically as a prerequisite for
+;;; closure conversion at the AST level.  The slot
+;;; VARIABLE-REFERENCE-AST contains a reference to the shared
+;;; variable, and the slot INDEX-AST contains a literal with a small
+;;; non-negative integer, corresponding to the position of the GO tag
+;;; in the TAGBODY.
+
 (define-ast-class go-with-variable-ast (ast)
   ((1 variable-reference-ast)
    (1 index-ast)))
