690fa56491c085893757a9b59d800f9c512c8bd4
Robert Strandh <robert.strandh@gmail.com>
2024-10-07 18:36:33 +0200

    Take the AST-INFO instance as an argument and put it in the client.

--- Code/AST-transformations/ast-ownership.lisp
+++ Code/AST-transformations/ast-ownership.lisp
@@ -9,28 +9,25 @@
 ;;; inside a LOCAL-FUNCTION-AST.
 
 (defclass ast-owners-client (client)
-  ((%owner-table :initform (make-hash-table :test #'eq)
-                 :reader owner-table)))
+  ((%ast-info :initarg :ast-info :reader ast-info)))
+
 
 (defvar *owner*)
 
 ;;; This method is used when AST is any AST class.
 (defmethod iaw:walk-ast-node :around ((client ast-owners-client) ast)
-  (setf (gethash ast (owner-table client)) *owner*)
+  (setf (gethash ast (owners (ast-info client))) *owner*)
   (call-next-method))
 
 (defmethod iaw:walk-ast-node :around
     ((client ast-owners-client) (ast ico:local-function-ast))
   (let ((*owner* ast))
     (call-next-method))
-  (setf (gethash ast (owner-table client)) *owner*)
+  (setf (gethash ast (owners (ast-info client))) *owner*)
   ast)
 
-(defun compute-owners (ast)
-  (let ((client (make-instance 'ast-owners-client))
+(defun compute-owners (ast ast-info)
+  (let ((client (make-instance 'ast-owners-client :ast-info ast-info))
         (*owner* nil))
     (iaw:walk-ast client ast)
     client))
-
-(defun owner (ast ast-owners)
-  (gethash ast (owner-table ast-owners)))
