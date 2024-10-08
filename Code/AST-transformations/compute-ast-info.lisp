a97b4215434e35b868c17b8c7d8635dcaf3ecf06
Robert Strandh <robert.strandh@gmail.com>
2024-10-08 17:17:49 +0200

    Call COPUTE-FUNCTION-TREE from COMPUTE-AST-INFO.

--- Code/AST-transformations/compute-ast-info.lisp
+++ Code/AST-transformations/compute-ast-info.lisp
@@ -4,6 +4,7 @@
   (let ((ast-info (make-instance 'ast-info)))
     (compute-parents ast ast-info)
     (compute-owners ast ast-info)
+    (compute-function-tree ast ast-info)
     (compute-escaped-functions ast ast-info)
     (compute-call-graph ast ast-info)
     ast-info))
