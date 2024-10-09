d1947436611ea75c45200e8a58bdf3cb19f6cb1f
Robert Strandh <robert.strandh@gmail.com>
2024-10-09 11:00:38 +0200

    Simplify code by using new accessors

--- Code/AST-transformations/function-tree.lisp
+++ Code/AST-transformations/function-tree.lisp
@@ -4,29 +4,21 @@
 ;;; corresponding to the nesting of local functions introduced by
 ;;; LABELS-ASTs.
 
-(defvar *parent-node*)
+(defvar *parent-function-ast*)
 
 (defclass function-tree-client (client)
-  (;; This table maps LOCAL-FUNCTION-ASTs to nodes.
-   (%node-table :initform (make-hash-table :test #'eq)
-                :reader node-table)
-   ;; This slot contains the root node of the function tree.
-   (%function-tree-root :initform *parent-node* :reader function-tree-node)))
+  ((%ast-info :initarg :ast-info :reader ast-info)))
 
 (defmethod iaw:walk-ast-node :around
     ((client function-tree-client) (ast ico:local-function-ast))
-  (let ((node (make-instance 'function-node
-                :node-function ast
-                :parent-node *parent-node*)))
-    (push node (child-nodes *parent-node*))
-    (setf (gethash ast (node-table client)) node)
-    (let ((*parent-node* node))
+  (let ((ast-info (ast-info client)))
+    (setf (function-parent-ast ast ast-info) *parent-function-ast*)
+    (push ast (function-child-asts *parent-function-ast* ast-info))
+    (let ((*parent-function-ast* ast))
       (call-next-method))))
 
 (defun compute-function-tree (ast ast-info)
-  (let* ((*parent-node* (make-instance 'function-node
-                          :node-function nil
-                          :parent-node nil))
-         (client (make-instance 'function-tree-client)))
+  (let* ((*parent-function-ast* nil)
+         (client (make-instance 'function-tree-client :ast-info ast-info)))
     (iaw:walk-ast client ast)
     client))
