97955363eb2e03b09be8f9fd07ef04198449b442
Robert Strandh <robert.strandh@gmail.com>
2024-10-19 16:56:27 +0200

    Add a slot containing a eference to the static environment.

--- Code/AST/static-environment-reference-ast.lisp
+++ Code/AST/static-environment-reference-ast.lisp
@@ -1,11 +1,14 @@
 (cl:in-package #:iconoclast)
 
-;;; This AST class is introduced by closure conversion.  A reference
-;;; to a shared variable is replaced by a reference to an element in
-;;; the static environment.
+;;; This AST class is introduced by closure conversion.  The value of
+;;; a shared variable can be accessed by the use of an AST of this
+;;; class.
 
-(define-ast-class static-environment-reference (ast)
-  (;; This slot contains a LITERAL-AST representing a non-negative
+(define-ast-class read-static-environment-ast (ast)
+  (;; This slot contains a VARIABLE-REFERENCE-AST referring to the
+   ;; static environment.
+   (1 static-environment-ast)
+   ;; This slot contains a LITERAL-AST representing a non-negative
    ;; integer that is a reference to an element of the the static
    ;; environment of the immediately enclosing LOCAL-FUNCTION-AST.
    (1 index-ast)))
