302c624160474ee2979eaa2064d6927270dcf6f0
Robert Strandh <robert.strandh@gmail.com>
2024-10-19 15:00:34 +0200

    Remove obsolete systems.

--- Code/Closure-conversion/packages.lisp
+++
@@ -1,9 +0,0 @@
-(cl:in-package #:common-lisp-user)
-
-(defpackage #:iconoclast-closure-conversion
-  (:use #:common-lisp)
-  (:local-nicknames (#:ico #:iconoclast)
-                    (#:aw #:iconoclast-ast-walker))
-  (:export #:tree-node-client
-           #:compute-function-tree
-           #:compute-ownership))
