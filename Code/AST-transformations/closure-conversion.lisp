110c3df20408828fea77a05bf857d92102592cdd
Robert Strandh <robert.strandh@gmail.com>
2024-10-19 15:52:50 +0200

    Beginning of description of algorithm for closure conversion.

--- Code/AST-transformations/closure-conversion.lisp
+++ Code/AST-transformations/closure-conversion.lisp
@@ -16,6 +16,37 @@
 ;;; reflects nesting.  Ownership and the function tree are computed by
 ;;; COMPUTE-AST-INFO and
 ;;;
-;;; For each shared variable, we have the function that defines it,
-;;; functions that references it, and perhaps some intermediate
-;;; functions that neither define nor reference it.
+;;; We handle each shared variable separately.  For each shared
+;;; variable, we have the function that defines it, functions that
+;;; references it, and perhaps some intermediate functions that
+;;; neither define nor reference it.  We consider the list of entries,
+;;; where an entry is either a variable definition or a variable
+;;; reference.
+;;;
+;;;   1. If every entry has the same owner, stop.
+;;;
+;;;   2. Consider the owners F1, F2, ... Fn of each entry.  Use the
+;;;      function tree to find the innermost function Fi of those
+;;;      owners.
+;;;
+;;;   3. Extract all entries (which are necessarily variable reference
+;;;      entries) with Fi as their owner, say R1, R2, ... Rk, and
+;;;      remove them from the original list.
+;;;
+;;;   4. If Fi is not in the list of function with a non-NIL static
+;;;      environment, then add a LET-TEMPORARY-AST with a
+;;;      STATIC-ENVIRONMENT-AST to the beginning of Fi.  Call the
+;;;      variable definition introduced S.  Add a
+;;;      SET-STATIC-ENVIRONMENT-AST to the beginning of the body of
+;;;      the LABELS-AST that defines Fi.  Add Fi to the list of
+;;;      functions with a non-NIL static environment.
+;;;
+;;;   5. Let Fj be parent of Fi in the function tree.  Create a new
+;;;      VARIABLE-REFERENCE-AST, say R, with Fj as the owner, and add
+;;;      it to the list of remaining entries.  Add R to the
+;;;      SET-STATIC-ENVIRONMENT-AST just introduced.
+;;;
+;;;   6. Create a new VARIABLE-DEFINITION-AST, say D, Add a
+;;;      LET-TEMPORARY-AST immediately as the first form of the
+;;;      LET-TEMPORARY-AST that introduces the static environment.
+;;;      Link up D and R1, R2, ... Rk.
