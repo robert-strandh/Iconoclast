5284c59b0fdd3cd55c08cf5a4e99938c823b8a92
Robert Strandh <robert.strandh@gmail.com>
2024-10-26 10:56:02 +0200

    Define function CHECK-AST.

--- Code/AST-transformations/compute-ast-info.lisp
+++ Code/AST-transformations/compute-ast-info.lisp
@@ -8,3 +8,21 @@
     (compute-escaped-functions ast ast-info)
     (compute-call-graph ast ast-info)
     ast-info))
+
+(defclass check-ast-client (client)
+  ((%ast-info :initarg :ast-info :reader ast-info)))
+
+(defmethod iaw:walk-ast-node :around
+    ((client check-ast-client) (ast ico:variable-definition-ast))
+  (call-next-method)
+  (let ((ast-info (ast-info client)))
+    (unless (null (owner-ast ast ast-info))
+      (loop for reference-ast in (ico:reference-asts ast)
+            for owner-ast = (owner-ast reference-ast ast-info)
+            do (assert (not (null owner-ast))))))
+  ast)
+
+(defun check-ast (ast ast-info)
+  (let ((client (make-instance 'check-ast-client :ast-info ast-info)))
+    (iaw:walk-ast client ast)))
+
