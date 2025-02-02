0af56b3c4d6ec487d2fee2a7beacea9a5ed13347
Robert Strandh <robert.strandh@gmail.com>
2025-02-02 08:06:55 +0100

    Distinguish between definitions and references.

--- Code/Visualizer/name-ast.lisp
+++ Code/Visualizer/name-ast.lisp
@@ -6,3 +6,17 @@
          (height 20))
     (draw-ast ast pane hpos vpos width height name)
     (+ vpos height 10)))
+
+(defmethod display-ast* ((ast ico:variable-definition-ast) pane hpos vpos)
+  (let* ((name (format nil "Def: ~a" (ico:name ast)))
+         (width (+ (clim:stream-string-width pane name) 10))
+         (height 20))
+    (draw-ast ast pane hpos vpos width height name)
+    (+ vpos height 10)))
+
+(defmethod display-ast* ((ast ico:variable-reference-ast) pane hpos vpos)
+  (let* ((name (format nil "Ref: ~a" (ico:name ast)))
+         (width (+ (clim:stream-string-width pane name) 10))
+         (height 20))
+    (draw-ast ast pane hpos vpos width height name)
+    (+ vpos height 10)))
