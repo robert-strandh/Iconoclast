c9cdc4360efde2dd24c0562823f78757bb285004
Robert Strandh <robert.strandh@gmail.com>
2025-02-04 12:33:49 +0100

    New component for visualizing TAGBODY-WITH-VARIABLE ASTs

---
+++ Code/Visualizer/tagbody-with-variable-ast.lisp
@@ -0,0 +1,31 @@
+(cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast* ((ast ico:tagbody-segment-ast) pane hpos vpos)
+  (let* ((name "segment")
+         (width (+ (clim:stream-string-width pane name) 10))
+         (height 20))
+    (draw-ast ast pane hpos vpos width height name)
+    (let ((child-vpos (+ vpos 30))
+          (tag-ast (ico:tag-ast ast)))
+      (unless (null tag-ast)
+        (setf child-vpos
+              (display-ast* tag-ast pane (+ hpos 20) child-vpos)))
+      (setf child-vpos
+            (display-asts
+             (ico:statement-asts ast) pane (+ hpos 30) child-vpos))
+      child-vpos)))
+
+(defmethod display-ast* ((ast ico:tagbody-with-variable-ast) pane hpos vpos)
+  (let* ((name "tagbody-with-variable")
+         (width (+ (clim:stream-string-width pane name) 10))
+         (height 20))
+    (draw-ast ast pane hpos vpos width height name)
+    (let ((child-vpos vpos))
+      (setf child-vpos
+            (display-ast* (ico:variable-definition-ast ast) pane
+                          (+ hpos width 20)
+                          child-vpos))
+      (setf child-vpos 
+            (display-asts (ico:segment-asts ast)
+                          pane (+ hpos 20) child-vpos))
+      child-vpos)))
