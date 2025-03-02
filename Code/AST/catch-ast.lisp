149f34fddcfc1f98d1c8a62bb8aec453f70e1a79
Robert Strandh <robert.strandh@gmail.com>
2025-03-02 10:35:49 +0100

    Replace TAG-AST-MIXIN by slot TAG-FORM-AST.

--- Code/AST/catch-ast.lisp
+++ Code/AST/catch-ast.lisp
@@ -1,9 +1,9 @@
 (cl:in-package #:iconoclast)
 
 (define-ast-class catch-ast
-    (implicit-progn-ast-mixin tag-ast-mixin special-form-ast)
-  ())
+    (implicit-progn-ast-mixin special-form-ast)
+  ((1 tag-form-ast)))
 
 (define-ast-class throw-ast
-    (required-form-ast-mixin tag-ast-mixin special-form-ast)
-  ())
+    (required-form-ast-mixin special-form-ast)
+  ((1 tag-form-ast)))
