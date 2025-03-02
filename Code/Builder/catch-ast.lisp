7e709ec29a98230702259430520e05c784652251
Robert Strandh <robert.strandh@gmail.com>
2025-03-02 10:36:20 +0100

    Replace accessor TAG-AST by TAG-FORM-AST.

--- Code/Builder/catch-ast.lisp
+++ Code/Builder/catch-ast.lisp
@@ -3,11 +3,11 @@
 (define-make-node-method :catch ico:catch-ast)
 
 (define-relations ico:catch-ast
-  ((:tag-form t ico:tag-ast)
+  ((:tag-form t ico:tag-form-ast)
    (:form t ico:form-asts)))
 
 (define-make-node-method :throw ico:throw-ast)
 
 (define-relations ico:throw-ast
-  ((:tag-form t ico:tag-ast)
+  ((:tag-form t ico:tag-form-ast)
    (:result-form t ico:form-ast)))
