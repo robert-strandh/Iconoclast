d90ea6a77bb647911c25a05a75454d03548169ce
Robert Strandh <robert.strandh@gmail.com>
2025-03-09 09:15:21 +0100

    Make BLOCK-AST and RETURN-FROM-AST respective superclasses.

--- Code/AST/block-with-variable-ast.lisp
+++ Code/AST/block-with-variable-ast.lisp
@@ -8,10 +8,16 @@
 ;;;; closure conversion discovers variable sharing using ordinary
 ;;;; variable AST classes.
 
-(define-ast-class block-with-variable-ast
-    (implicit-progn-ast-mixin special-form-ast)
+(define-ast-class block-with-variable-ast (block-ast)
   ((1 variable-definition-ast)))
 
-(define-ast-class return-from-with-variable-ast
-    (optional-form-ast-mixin special-form-ast)
+(define-ast-class return-from-with-variable-ast (return-from-ast)
   ((1 variable-reference-ast)))
+
+;; (define-ast-class block-with-variable-ast
+;;     (implicit-progn-ast-mixin special-form-ast)
+;;   ((1 variable-definition-ast)))
+
+;; (define-ast-class return-from-with-variable-ast
+;;     (optional-form-ast-mixin special-form-ast)
+;;   ((1 variable-reference-ast)))
