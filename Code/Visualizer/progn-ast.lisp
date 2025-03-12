33fc8abd7be95bea777a296d5590435b51afaf1a
Robert Strandh <robert.strandh@gmail.com>
2025-03-12 14:07:54 +0100

    Define method on DISPLAY-AST for PROGN-AST.

--- Code/Visualizer/progn-ast.lisp
+++ Code/Visualizer/progn-ast.lisp
@@ -1,5 +1,13 @@
 (cl:in-package #:iconoclast-visualizer)
 
+(defmethod display-ast ((ast ico:progn-ast))
+  (let* ((name "progn")
+         (width (+ (clim:stream-string-width *pane* name) 10))
+         (height 20))
+    (draw-ast ast *pane* 0 0 width height name)
+    (clim:with-translation (*pane* 10 (+ height 10))
+      (display-asts (ico:form-asts ast)))))
+
 (defmethod display-ast* ((ast ico:progn-ast) pane hpos vpos)
   (let* ((name "progn")
          (width (+ (clim:stream-string-width pane name) 10))
