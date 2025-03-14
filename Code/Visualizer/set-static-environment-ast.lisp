b06aec66f048bce1a228f10279c9ac1db2e72a3c
Robert Strandh <robert.strandh@gmail.com>
2025-03-14 17:11:24 +0100

    Provide method on DISPLAY-AST for SET-STATIC-ENVIRONMENT-AST.

--- Code/Visualizer/set-static-environment-ast.lisp
+++ Code/Visualizer/set-static-environment-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:set-static-environment-ast))
+  (draw-ast ast "set-static-environment")
+  (with-indentation (40)
+    (display-ast (ico:function-reference-ast ast)))
+  (with-indentation (20)
+    (display-asts (ico:form-asts ast))))
