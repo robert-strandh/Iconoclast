b4c909140c7d0aeaf1d560348a54da399e22c980
Robert Strandh <robert.strandh@gmail.com>
2025-03-14 07:59:29 +0100

    Simplify WITH-CHILD-ASTS.

--- Code/Visualizer/utilities.lisp
+++ Code/Visualizer/utilities.lisp
@@ -1,13 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
 
 (defmacro with-child-asts ((delta-hpos) &body body)
-  (let ((delta-hpos-variable (gensym)))
-    `(let ((,delta-hpos-variable ,delta-hpos))
-       ,@(loop for form in body
-               collect `(clim:with-translation
-                            (*pane* ,delta-hpos-variable 0)
-                          ,form))
-       0)))
+  `(clim:with-translation (*pane* ,delta-hpos 0)
+     ,@body))
 
 (defun string-width (string)
   (clim:stream-string-width *pane* string))
