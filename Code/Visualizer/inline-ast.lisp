66f619aa2e489cf62b4242080ed341712502ee89
Robert Strandh <robert.strandh@gmail.com>
2025-03-15 10:39:53 +0100

    Supply methods on DISPLAY-AST for INLINE-AST and NOTINLINE-AST.

--- Code/Visualizer/inline-ast.lisp
+++ Code/Visualizer/inline-ast.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:inline-ast))
+  (draw-ast ast "inline")
+  (with-indentation (20)
+    (display-asts (ico:name-asts ast))))
+
+(defmethod display-ast ((ast ico:notinline-ast))
+  (draw-ast ast "notinline")
+  (with-indentation (20)
+    (display-asts (ico:name-asts ast))))
