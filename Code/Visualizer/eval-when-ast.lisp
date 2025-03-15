4c23eedd7bf2525bf231f19da501a63f32c56750
Robert Strandh <robert.strandh@gmail.com>
2025-03-15 10:38:33 +0100

    Supply method on DISPLAY-AST for EVAL-WHEN-AST

--- Code/Visualizer/eval-when-ast.lisp
+++ Code/Visualizer/eval-when-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:eval-when-ast))
+  (draw-ast ast "eval-when")
+  (with-indentation (40)
+    (display-asts (ico:situation-asts ast)))
+  (with-indentation (20)
+    (display-asts (ico:form-asts ast))))
