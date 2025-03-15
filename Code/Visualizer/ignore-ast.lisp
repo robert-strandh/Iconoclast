e18255731a25b4b5f17488e99653f301b3396f78
Robert Strandh <robert.strandh@gmail.com>
2025-03-15 10:39:25 +0100

    Supply methods on DISPLAY-AST for IGNORE-AST and IGNORABLE-AST.

--- Code/Visualizer/ignore-ast.lisp
+++ Code/Visualizer/ignore-ast.lisp
@@ -1,1 +1,11 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:ignore-ast))
+  (draw-ast ast "ignore")
+  (with-indentation (20)
+    (display-asts (ico:name-asts ast))))
+
+(defmethod display-ast ((ast ico:ignorable-ast))
+  (draw-ast ast "ignorable")
+  (with-indentation (20)
+    (display-asts (ico:name-asts ast))))
