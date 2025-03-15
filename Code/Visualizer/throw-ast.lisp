961feea841776cfd151b2f8d942230f2a542163b
Robert Strandh <robert.strandh@gmail.com>
2025-03-15 10:27:13 +0100

    Supply method on DISPLAY-AST for THROW-AST

--- Code/Visualizer/throw-ast.lisp
+++ Code/Visualizer/throw-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:throw-ast))
+  (draw-ast ast "throw")
+  (with-indentation (40)
+    (display-ast (ico:tag-form-ast ast)))
+  (with-indentation (20)
+    (display-ast (ico:form-ast ast))))
