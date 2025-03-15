3866a6517e883e1972df39aa534dcb845530f23b
Robert Strandh <robert.strandh@gmail.com>
2025-03-15 10:26:49 +0100

    Supply method on DISPLAY-AST for CATCH-AST

--- Code/Visualizer/catch-ast.lisp
+++ Code/Visualizer/catch-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:catch-ast))
+  (draw-ast ast "catch")
+  (with-indentation (40)
+    (display-ast (ico:tag-form-ast ast)))
+  (with-indentation (20)
+    (display-asts (ico:form-asts ast))))
