c512541d8790bd5cda874f7cc15aa9a608f00003
Robert Strandh <robert.strandh@gmail.com>
2025-03-15 10:38:50 +0100

    Supply method on DISPLAY-AST for FUNCTION-AST

--- Code/Visualizer/function-ast.lisp
+++ Code/Visualizer/function-ast.lisp
@@ -1,1 +1,6 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:function-ast))
+  (draw-ast ast "function")
+  (with-indentation (20)
+    (display-ast (ico:name-ast ast))))
