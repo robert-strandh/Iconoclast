c225844d0352c7589bf338c575de033e108c426b
Robert Strandh <robert.strandh@gmail.com>
2025-03-16 07:17:18 +0100

    Provide method on DISPLAY-AST for LOCALLY-AST.

--- Code/Visualizer/locally-ast.lisp
+++ Code/Visualizer/locally-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:locally-ast))
+  (draw-ast ast "locally")
+  (with-indentation (40)
+    (display-asts (ico:declaration-asts ast)))
+  (with-indentation (20)
+    (display-asts (ico:form-asts ast))))
