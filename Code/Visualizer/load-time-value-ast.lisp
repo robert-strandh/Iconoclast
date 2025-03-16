c66b00c3f57034876356f35115d2d4f55b10f9f5
Robert Strandh <robert.strandh@gmail.com>
2025-03-16 13:30:19 +0100

    Supply method on DISPLAY-AST for LOAD-TIME-VALUE-AST.

--- Code/Visualizer/load-time-value-ast.lisp
+++ Code/Visualizer/load-time-value-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:load-time-value-ast))
+  (draw-ast ast "load-time-value")
+  (with-indentation (30)
+    (display-ast (ico:read-only-p-ast ast)))
+  (with-indentation (20)
+    (display-ast (ico:form-ast ast))))
