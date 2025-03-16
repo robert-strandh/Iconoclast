d06717e10b2983780fbc7574ca4fb2ab1700f3ae
Robert Strandh <robert.strandh@gmail.com>
2025-03-16 13:32:32 +0100

    Supply method on DISPLAY-AST for MULTIPLE-VALUE-CALL-AST.

--- Code/Visualizer/multiple-value-call-ast.lisp
+++ Code/Visualizer/multiple-value-call-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:multiple-value-call-ast))
+  (draw-ast ast "multiple-value-call")
+  (with-indentation (40)
+    (display-ast (ico:function-ast ast)))
+  (with-indentation (20)
+    (display-asts (ico:form-asts ast))))
