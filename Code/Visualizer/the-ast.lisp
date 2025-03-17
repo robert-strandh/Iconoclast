9207b39c0b765766c50c05000a9751336387cac4
Robert Strandh <robert.strandh@gmail.com>
2025-03-17 05:16:35 +0100

    Supply method on DISPLAY-AST for THE-AST.

--- Code/Visualizer/the-ast.lisp
+++ Code/Visualizer/the-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:the-ast))
+  (draw-ast ast "the")
+  (with-indentation (40)
+    (display-ast (ico:value-type-ast ast)))
+  (with-indentation (20)
+    (display-ast (ico:form-ast ast))))
