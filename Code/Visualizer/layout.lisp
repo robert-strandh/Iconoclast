e28ff2e9a3372a52edba2f11e7759a11c2970f4e
Robert Strandh <robert.strandh@gmail.com>
2025-03-17 05:08:06 +0100

    Remove obsolete LAYOUT methods now handled by DISPLAY-AST.

--- Code/Visualizer/layout.lisp
+++ Code/Visualizer/layout.lisp
@@ -5,16 +5,6 @@
 (defmethod layout (ast)
   '())
 
-(defmethod layout ((ast ico:multiple-value-bind-ast))
-  '((40 ico:variable-name-asts)
-    (30 ico:values-ast)
-    (20 ico:declaration-asts)
-    (10 ico:form-asts)))
-
-(defmethod layout ((ast ico:application-ast))
-  '((20 ico:function-name-ast)
-    (30 ico:argument-asts)))
-
 (defmethod layout ((ast ico:lambda-expression-ast))
   '((40 ico:lambda-list-ast)
     (30 ico:declaration-asts)
