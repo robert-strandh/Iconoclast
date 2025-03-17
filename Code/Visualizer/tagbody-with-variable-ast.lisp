5de0c488291499dddd918d66043a7701b5ca943c
Robert Strandh <robert.strandh@gmail.com>
2025-03-17 05:25:39 +0100

    Supply method on DISPLAY-AST for TAGBODY-WITH-VARIABLE-AST.

--- Code/Visualizer/tagbody-with-variable-ast.lisp
+++ Code/Visualizer/tagbody-with-variable-ast.lisp
@@ -1,1 +1,8 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:tagbody-with-variable-ast))
+  (draw-ast ast "tagbody-with-variable")
+  (with-indentation (40)
+    (display-ast (ico:variable-definition-ast ast)))
+  (with-indentation (20)
+    (display-asts (ico:segment-asts ast))))
