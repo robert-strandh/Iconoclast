a468e9818896a5a2dd54b4b847fd46b6dde604b9
Robert Strandh <robert.strandh@gmail.com>
2025-03-17 05:27:42 +0100

    Supply method on DISPLAY-AST for TAGBODY-AST.

--- Code/Visualizer/tagbody-ast.lisp
+++ Code/Visualizer/tagbody-ast.lisp
@@ -1,1 +1,6 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:tagbody-ast))
+  (draw-ast ast "tagbody")
+  (with-indentation (20)
+    (display-asts (ico:segment-asts ast))))
