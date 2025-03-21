379a0fc8179e0b5c16390d0beb2598268a37cc1e
Robert Strandh <robert.strandh@gmail.com>
2025-03-21 16:41:34 +0100

    Provide methods for SPECIAL-VARIABLE-BING/IN-AST.

--- Code/Visualizer/special-variable-bind-ast.lisp
+++ Code/Visualizer/special-variable-bind-ast.lisp
@@ -1,1 +1,15 @@
 (cl:in-package #:iconoclast-visualizer)
+
+(defmethod display-ast ((ast ico:special-variable-binding-ast))
+  (draw-ast ast "special-variable-bind")
+  (with-indentation (40)
+    (display-ast (ico:variable-name-ast ast)))
+  (with-indentation (20)
+    (display-ast (ico:form-ast ast))))
+
+(defmethod display-ast ((ast ico:special-variable-bind-ast))
+  (draw-ast ast "special-variable-bind")
+  (with-indentation (40)
+    (display-ast (ico:binding-ast ast)))
+  (with-indentation (20)
+    (display-asts (ico:form-asts ast))))
