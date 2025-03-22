e51dc9c8c6f956b8927cf7e05444d9d8b4af12b7
Robert Strandh <robert.strandh@gmail.com>
2025-03-22 06:00:11 +0100

    Remove obsolete methods on LAYOUT.

--- Code/Visualizer/layout.lisp
+++ Code/Visualizer/layout.lisp
@@ -10,37 +10,6 @@
     (30 ico:declaration-asts)
     (20 ico:form-asts)))
 
-(defmethod layout ((ast ico:single-parameter-section-ast))
-  `((20 ico:parameter-ast)))
-
-(defmethod layout ((ast ico:multi-parameter-section-ast))
-  `((20 ico:parameter-asts)))
-
-(defmethod layout ((ast ico:required-parameter-ast))
-  '((20 ico:name-ast)))
-
-(defmethod layout ((ast ico:optional-parameter-ast))
-  `((20 ico:name-ast)
-    (30 ico:init-form-ast)
-    (40 ico:supplied-p-parameter-ast)))
-
-(defmethod layout ((ast ico:key-parameter-ast))
-  `((20 ico:keyword-ast)
-    (30 ico:name-ast)
-    (40 ico:init-form-ast)
-    (50 ico:supplied-p-parameter-ast)))
-
-(defmethod layout ((ast ico:aux-parameter-ast))
-  `((20 ico:name-ast)
-    (30 ico:form-ast)))
-
-(defmethod layout ((ast ico:ordinary-lambda-list-ast))
-  `((20 ico:required-section-ast)
-    (20 ico:optional-section-ast)
-    (20 ico:rest-section-ast)
-    (20 ico:key-section-ast)
-    (20 ico:aux-section-ast)))
-
 (defmethod layout ((ast ico:when-ast))
   `((40 ico:test-ast)
     (20 ico:form-asts)))
